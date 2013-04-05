23a0b0b91642f903dbf4f12a6ed930aabfbb5b04
Raphaël Londeix <raphael.londeix@gmail.com>
2013-04-05 15:10:59 +0200

    d00m: add option and fallback to select the user

--- satellites/d00m/8d00m.py
+++ satellites/d00m/8d00m.py
@@ -25,38 +25,52 @@ def on_canceled(state, transaction, new):
             state.running = False
 
 def login(state, email = None):
-    receiver_id = os.getenv("INFINIT_USER", None)
-    if receiver_id == None:
-        raise Exception("you must provide INFINIT_USER")
+    if email is None:
+        email = os.environ.get("INFINIT_USER")
+    if email is None:
+        email = input("email: ").strip()
+    else:
+        print("email:", email)
     password = getpass("password: ")
-    state.login(receiver_id, password)
+    state.login(email, password)
     state.connect()
-    return receiver_id
+    return email
 
 
-def main(state):
-    id = login(state)
+def main(state, email):
+    email = login(state, email)
 
     state.transaction_status_callback(partial(on_canceled, state))
 
     state.running = True
-    state.set_device_name(id + "device")
+    state.set_device_name(email + "-device")
 
     transactions = state.transactions()
     state.number_of_transactions = len(transactions)
 
     for transaction_id in transactions:
-        print("doom transaction {}".format(transaction_id))
+        print("\U0001f63b  doomed transaction {}".format(transaction_id))
         state.update_transaction(transaction_id, state.TransactionStatus.canceled)
     else:
         return
 
+    networks = state.networks()
+    for network in networks:
+        print(network)
+        state.delete_network(network)
     while state.running:
         state.poll()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(sys.argv[0], description="Clean user's transactions and networks")
     parser.add_argument("-l", "--logfile", help="path to the log file")
+    parser.add_argument(
+        '-u', '--user',
+        help = "Specify user to clean",
+        action = "store",
+        default = os.environ.get("INFINIT_USER"),
+    )
+    parser.add_argument('-n', '--networks', help="Destroy all user's networks")
     args = parser.parse_args()
 
     if args.logfile:
@@ -66,9 +80,9 @@ if __name__ == "__main__":
     state = gap.State()
 
     try:
-        main(state)
+        main(state, args.user)
     except KeyboardInterrupt as e:
         if getattr(state, "current_transaction_id", None):
             tid = state.current_transaction_id
-            print("Interupted.")
+            print("Interrupted.")
     del state
