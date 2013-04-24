b45d8102ea67490dbb6e821b75df5cc6a7e4ba91
Raphaël Londeix <raphael.londeix@gmail.com>
2013-04-24 13:47:40 +0200

    fix 8send: rethrow exception caught

--- satellites/sendto/8sendto.py
+++ satellites/sendto/8sendto.py
@@ -111,12 +111,11 @@ if __name__ == "__main__":
         except KeyboardInterrupt as e:
             if getattr(state, "current_transaction_id", None):
                 tid = state.current_transaction_id
-                print("Interupted.")
-                print("Cancel the outgoing transaction ({})".format(tid))
+                print("Interrupted. Cancel the outgoing transaction ({})".format(tid))
                 state.update_transaction(tid, state.TransactionStatus.canceled)
         except Exception as e:
             if getattr(state, "current_transaction_id", None):
                 tid = state.current_transaction_id
-                print("Interupted. Cancel the outgoing transaction ({})".format(tid))
+                print("Interrupted. Cancel the outgoing transaction ({})".format(tid))
                 state.update_transaction(tid, state.TransactionStatus.canceled)
-                raise e
+            raise e
