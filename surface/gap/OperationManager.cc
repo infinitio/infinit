29b9a1d3fb4198deb1bed8d030c4d9d1cb6280f0
Raphaël Londeix <raphael.londeix@infinit.io>
2013-05-18 17:00:12 +0200

    surface/gap: Clean & document OperationManager. Factorize exception handling.

--- surface/gap/OperationManager.cc
+++ surface/gap/OperationManager.cc
@@ -4,12 +4,14 @@
 
 #include <elle/Exception.hh>
 
-ELLE_LOG_COMPONENT("infinit.surface.gap.State");
+ELLE_LOG_COMPONENT("infinit.surface.gap.OperationManager");
 
 namespace surface
 {
   namespace gap
   {
+
+    //- OperationManager ------------------------------------------------------
 
     OperationManager::OperationStatus
     OperationManager::status(OperationId const id) const
@@ -97,6 +99,89 @@ namespace surface
           return;
         }
       }
+    }
+
+    //- Operation -------------------------------------------------------------
+
+    OperationManager::Operation::Operation(std::string const& name)
+      : _name{name}
+      , _done{false}
+      , _succeeded{false}
+      , _cancelled{false}
+      , _delete_later{false}
+      , _rethrown{false}
+      , _exception{}
+      , _failure_reason{}
+    {}
+
+    OperationManager::Operation::~Operation()
+    {}
+
+    void
+    OperationManager::Operation::cancel()
+    {
+      ELLE_LOG_COMPONENT("infinit.surface.gap.Operation");
+      ELLE_TRACE_METHOD(this->_name);
+      if (this->_done || this->_cancelled)
+        return;
+      this->_cancelled = true;
+      this->_cancel();
+    }
+
+    void
+    OperationManager::Operation::_cancel()
+    {
+      ELLE_LOG_COMPONENT("infinit.surface.gap.Operation");
+      ELLE_TRACE_FUNCTION(this->_name);
+    }
+
+    void
+    OperationManager::Operation::rethrow()
+    {
+      ELLE_LOG_COMPONENT("infinit.surface.gap.Operation");
+      ELLE_TRACE_FUNCTION(this->_name);
+
+      this->_rethrown = true;
+      std::rethrow_exception(this->_exception);
+    }
+
+    std::string
+    OperationManager::Operation::_exception_string(std::exception_ptr eptr)
+    {
+      if (!eptr)
+        eptr = std::current_exception();
+      if (!eptr)
+        throw elle::Exception{"no current exception present"};
+      try
+      {
+        std::rethrow_exception(eptr);
+      }
+      catch (elle::Exception const& e)
+      {
+        return elle::sprint(e);
+      }
+      catch (std::exception const& e)
+      {
+        return e.what();
+      }
+      catch (...)
+      {
+        return "unknown exception type";
+      }
+
+      elle::unreachable();
+    }
+
+    std::string
+    OperationManager::Operation::failure_reason()
+    {
+      if (!this->_exception)
+        throw elle::Exception{"no current exception"};
+
+      if (this->_failure_reason.empty())
+        this->_failure_reason = this->_exception_string(this->_exception);
+
+      return this->_failure_reason;
     }
   }
 }
