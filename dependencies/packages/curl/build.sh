997807fb99741363f658705b249f9f014678c051
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-05-23 12:23:37 +0200

    Dependencies: Embed curl into the repository, with right rpath.

---
+++ dependencies/packages/curl/build.sh
@@ -0,0 +1,53 @@
+#
+# ---------- globals ----------------------------------------------------------
+#
+
+SCRIPT=`basename "${0}"`
+ROOTDIR=`python -c "import os;print(os.path.abspath(os.path.dirname('$0')))"`
+
+#
+# ---------- imports ----------------------------------------------------------
+#
+
+BASEDIR="${ROOTDIR}/../.."
+
+. "${BASEDIR}/environment.sh"
+. "${BASEDIR}/common.sh"
+
+#
+# ---------- entry point ------------------------------------------------------
+#
+
+uptodate "${CURL_LIBRARIES}" ||
+(
+    rm -Rf "${BUILDDIR}/${CURL_NAME}"
+
+    download "${CURL_SNAPSHOT}" "${CURL_FINGERPRINT}" "${BUILDDIR}/${CURL_TARBALL}"
+
+    cd "${BUILDDIR}" ||
+    die "unable to move to the build directory '${BUILDDIR}'"
+
+    tar xzf "${CURL_TARBALL}" ||
+    die "unable to extract the tarball"
+
+    cd "${CURL_NAME}" ||
+    die "unable to enter the directory"
+
+    ./configure                                         \
+        --prefix="${WORKDIR}"                           \
+        --with-ssl="${WORKDIR}"                         \
+        --enable-optimize                               \
+        --enable-warnings                               \
+        --disable-ldap                                  \
+        --disable-ldaps                                 \
+        --disable-rtmp                                  \
+        --disable-sspi                                  \
+        --disable-rtsp                                  \
+        --disable-rtmp || die "unable to configure"
+    make all install || die "unable to build"
+    for lib in ${WORKDIR}/lib/libcurl.so*
+    do
+      echo patchelf --set-rpath \$ORIGIN $lib
+      patchelf --set-rpath \$ORIGIN $lib
+    done
+)
