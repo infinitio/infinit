ac7f02d6bce8d310781e06d56abf231b3d93c973
mefyl <quentin.hocquet@infinit.io>
2013-05-27 14:15:11 +0200

    hole.slug: Test pushing and pulling back a block.

--- tests/unit/hole/slug.cc
+++ tests/unit/hole/slug.cc
@@ -1,21 +1,26 @@
 #define BOOST_TEST_MODULE Hole
 #define BOOST_TEST_DYN_LINK
+
 #include <boost/test/unit_test.hpp>
 
 #include <cryptography/KeyPair.hh>
 
 #include <elle/serialize/extract.hh>
 
-#include <common/common.hh>
+#include <reactor/scheduler.hh>
+#include <reactor/thread.hh>
 
+#include <common/common.hh>
 #include <hole/Hole.hh>
 #include <hole/implementations/slug/Slug.hh>
 #include <hole/storage/Memory.hh>
 #include <hole/Passport.hh>
+#include <nucleus/factory.hh>
+#include <nucleus/neutron/Component.hh>
+#include <nucleus/neutron/Group.hh>
+#include <nucleus/proton/ContentHashBlock.hh>
 #include <nucleus/proton/Network.hh>
 #include <Infinit.hh>
-#include <reactor/scheduler.hh>
-#include <reactor/thread.hh>
 
 void
 Main()
@@ -37,12 +42,21 @@ Main()
       mem, passport, authority,
       reactor::network::Protocol::tcp, members, 0,
       boost::posix_time::milliseconds(5000)));
+
+  nucleus::neutron::Group g(n, keys.K(), "towel");
+  g.seal(keys.k());
+  auto addr = g.bind();
+  h->push(addr, g);
+
+  auto pulled = h->pull(addr, nucleus::proton::Revision::Last);
+  auto pulled_group = dynamic_cast<nucleus::neutron::Group*>(pulled.get());
+  BOOST_CHECK(pulled_group);
+  BOOST_CHECK_EQUAL(pulled_group->description(), "towel");
 }
 
 BOOST_AUTO_TEST_CASE(test_hole)
 {
   reactor::Scheduler sched;
-
   reactor::Thread t(sched,
                     "main",
                     [&] {Main();});
