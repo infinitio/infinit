2b5a3170044b59497358ba5721962ce468f17895
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-05-30 11:01:33 +0200

    heartbeat: Norm.

--- HeartBeat.cc
+++ HeartBeat.cc
@@ -13,49 +13,49 @@ ELLE_LOG_COMPONENT("infinit.heartbeat");
 
 namespace infinit
 {
-    namespace network = reactor::network;
+  namespace network = reactor::network;
 
-    reactor::Thread*
-    start_heartbeat(network::UDPSocket& sock)
+  reactor::Thread*
+  start_heartbeat(network::UDPSocket& sock)
+  {
+    auto& sched = *reactor::Scheduler::scheduler();
+    auto heartbite = [&]
     {
-      auto& sched = *reactor::Scheduler::scheduler();
-      auto heartbite = [&]
+      namespace network = reactor::network;
+
+      auto* ptr = &sock;
+      auto th_sleep = [&] (int sec)
       {
-        namespace network = reactor::network;
-
-        auto* ptr = &sock;
-        auto th_sleep = [&] (int sec)
-        {
-          reactor::Sleep s{sched, boost::posix_time::seconds{sec}};
-          s.run();
-        };
-
-        try
-        {
-          network::UDTSocket hsocket(sched,
-                                    *ptr,
-                                    common::longinus::host(), "9898");
-
-          for (int i = 0; i < 10; ++i)
-          {
-            std::string msg{"echo"};
-            hsocket.write(network::Buffer{msg});
-            msg.resize(512);
-            size_t bytes = hsocket.read_some(network::Buffer{msg});
-            msg.resize(bytes);
-            ELLE_DUMP("received %s", msg);
-            th_sleep(5);
-          }
-        }
-        catch (network::ConnectionClosed const&)
-        {
-          ELLE_LOG("lose connection with %s", socket);
-        }
-        catch (elle::Exception const& e)
-        {
-          ELLE_LOG("exception handled: %s", e.what());
-        }
+        reactor::Sleep s{sched, boost::posix_time::seconds{sec}};
+        s.run();
       };
-      return new reactor::Thread{sched, "heartbite", std::move(heartbite)};
-    }
+
+      try
+      {
+        network::UDTSocket hsocket(sched,
+                                  *ptr,
+                                  common::longinus::host(), "9898");
+
+        for (int i = 0; i < 10; ++i)
+        {
+          std::string msg{"echo"};
+          hsocket.write(network::Buffer{msg});
+          msg.resize(512);
+          size_t bytes = hsocket.read_some(network::Buffer{msg});
+          msg.resize(bytes);
+          ELLE_DUMP("received %s", msg);
+          th_sleep(5);
+        }
+      }
+      catch (network::ConnectionClosed const&)
+      {
+        ELLE_LOG("lose connection with %s", socket);
+      }
+      catch (elle::Exception const& e)
+      {
+        ELLE_LOG("exception handled: %s", e.what());
+      }
+    };
+    return new reactor::Thread{sched, "heartbite", std::move(heartbite)};
+  }
 } /* infinit */
