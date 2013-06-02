85670bb66cb9cbd2eddde40b1cc06f6e1bebbed0
Antony MECHIN <antony.mechin@infinit.io>
2013-06-02 21:23:26 +0200

    surface.crust: Add a python wrapper.

---
+++ surface/crust/python/Network.cc
@@ -0,0 +1,21 @@
+#include <wrappers/boost/python.hh>
+#include <surface/crust/Network.hh>
+
+BOOST_PYTHON_MODULE(_crust)
+{
+  namespace py = boost::python;
+  typedef py::return_value_policy<py::return_by_value> by_value;
+  typedef py::return_value_policy<py::copy_const_reference> by_cref;
+
+  py::class_<Network>("Network",
+                      boost::python::init<std::string const&,
+                                          std::string const&,
+                                          std::string const&,
+                                          std::string const&,
+                                          std::string const&,
+                                          std::string const&>());
+    // .def("sign", &Network::seal)
+    // .def("validate", &Network::validate)
+    // .def("create", &Network::create, by_cref());
+    ;
+}
