c9d670654d46e6272c8fb415a6ce8e5a50d27792
Raphaël Londeix <raphael.londeix@infinit.io>
2013-06-13 13:37:33 +0200

    surface.gap: Factor out binary process config.

---
+++ surface/gap/binary_config.cc
@@ -0,0 +1,33 @@
+#include "binary_config.hh"
+
+#include <common/common.hh>
+
+#include <boost/filesystem.hpp>
+
+namespace surface
+{
+  namespace gap
+  {
+    namespace fs = boost::filesystem;
+
+    /// @brief Return a valid config process for an infinit binary.
+    elle::system::ProcessConfig
+    binary_config(std::string const& name,
+                  std::string const& user_id,
+                  std::string const& network_id)
+    {
+      using namespace elle::system;
+      auto config = process_config(elle::system::normal_config);
+      std::string path = (
+        fs::path(common::infinit::network_directory(user_id, network_id)) /
+        (name + ".log")).string();
+      config.pipe_file(ProcessChannelStream::out, path);
+      config.pipe_file(ProcessChannelStream::err, path);
+      config.setenv("ELLE_LOG_PID", "1");
+      config.setenv("ELLE_LOG_TID", "1");
+      config.setenv("ELLE_LOG_TIME", "1");
+      config.setenv("ELLE_LOG_LEVEL", "DEBUG");
+      return config;
+    }
+  }
+}
---
+++ surface/gap/binary_config.hh
@@ -0,0 +1,18 @@
+#ifndef SURFACE_GAP_BINARY_CONFIG_HH
+# define SURFACE_GAP_BINARY_CONFIG_HH
+
+# include <elle/system/Process.hh>
+
+namespace surface
+{
+  namespace gap
+  {
+    /// @brief Return a valid config process for an infinit binary.
+    elle::system::ProcessConfig
+    binary_config(std::string const& name,
+                  std::string const& user_id,
+                  std::string const& network_id);
+  }
+}
+
+#endif
