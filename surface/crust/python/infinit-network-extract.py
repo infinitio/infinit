ac6646ad1352b79f1526021a0e64d042e5a6d4d1
Antony MECHIN <antony.mechin@infinit.io>
2013-06-19 22:40:49 +0200

    surface.crust.Network: Split retrieve script into retrieve(remote) and extract(local).

--- surface/crust/python/infinit-network-extract.py
+++ surface/crust/python/infinit-network-extract.py
@@ -3,41 +3,43 @@
 
 import infinit_utils
 
-def remote_retrieve(owner_name, network_name, host, port, token_path, descriptor_path):
-    from pycrust import Network, ID
-
-    identifier = Network.lookup(owner_name, network_name, host, port, token_path)
-    network = Network(ID(identifier), host, port, token_path)
+def local_retrieve(user_name, network_name, home, descriptor_path):
+    from pycrust import Network
+    network = Network(user_name, network_name, home)
 
     if descriptor_path:
         network.store(descriptor_path)
 
 def main(args):
-    print(args)
     from os import path
+    home = args.infinit_home or getenv("INFINIT_HOME")
+    if not home:
+        raise Exception("You must provide a home directory, with --infinit-home or env INFINIT_HOME")
+    if not args.user_name:
+        raise Exception("You must provide a --user-name, it's tempory.")
     if not args.store_local_descriptor_path:
         raise Exception("You must provide a --store-local-descriptor-path.")
     if args.store_local_descriptor_path and not args.force and path.exists(args.store_local_descriptor_path):
         raise Exception("Descriptor storing destination already exists and you didn't specify --force option.")
 
-    meta_host, meta_port, meta_token_path = infinit_utils.meta_values(args)
-
-    remote_retrieve(owner_name = args.owner_name or "",
-                    network_name = args.network_name,
-                    host = meta_host,
-                    port = meta_port,
-                    token_path = meta_token_path,
-                    descriptor_path = args.store_local_descriptor_path)
+    local_retrieve(args.user_name,
+                   args.network_name,
+                   args.infinit_home,
+                   args.store_local_descriptor_path)
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
+    # Local data.
     parser.add_argument("network_name",
                         help = "XXX: The name you gave to the network")
-    parser.add_argument("--owner-name",
-                        help = "XXX: The owner of the network. If not provided, it's you.")
-    infinit_utils.meta_to_parser(parser)
+    parser.add_argument("--infinit-home",
+                        help = "XXX: The path to your infinit home directory")
+    parser.add_argument("--user-name",
+                        help = "XXX: WILL BE REMOVE, IT'S YOU, IT'S YOUR NAME")
+
+    # Storing.
     parser.add_argument("--store-local-descriptor-path",
                         help = "The path where the descriptor will be save.")
     parser.add_argument("--force",
