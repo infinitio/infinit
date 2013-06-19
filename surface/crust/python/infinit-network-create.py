f8f263cb8f85c2333f1d040ff20f6f15ca8c3d76
Antony MECHIN <antony.mechin@infinit.io>
2013-06-19 22:44:17 +0200

    surface.crust.python: Change mandatory field name to optional description for network creation.

--- surface/crust/python/infinit-network-create.py
+++ surface/crust/python/infinit-network-create.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
 
-def create(name, identity_path, model, openness, policy, descriptor_path, force):
+def create(identity_path, model, openness, policy, description, descriptor_path, force):
     from getpass import getpass
     password = getpass("Identity password: ")
     from pycrust import Network
 
-    net = Network(name, identity_path, password, model, openness, policy)
+    net = Network(identity_path, password, model, openness, policy, description)
 
     if descriptor_path:
       net.store(descriptor_path, force or False)
@@ -21,12 +21,12 @@ def main(args):
     if args.store_local_descriptor_path and not args.force and path.exists(args.store_local_descriptor_path):
         raise Exception("Descriptor storing destination already exists and you didn't specify --force option.")
 
-    id = create(name = args.NAME,
-                identity_path = identity_path,
+    id = create(identity_path = identity_path,
                 model = args.model,
                 openness = args.openness,
                 policy = args.policy,
                 descriptor_path = args.store_local_descriptor_path,
+                description = args.description or "",
                 force = args.force)
 
     print(id)
@@ -35,8 +35,6 @@ if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("NAME",
-                        help = "The name of the network you want to create. It has to be unique for you.")
     parser.add_argument("--identity-path",
                         help = "The path to your identity file. You can also export INFINIT_IDENTITY_PATH.")
     parser.add_argument("--model",
@@ -51,6 +49,9 @@ if __name__ == "__main__":
                         choices = ['open', 'community', 'closed'],
                         default = 'open',
                         help = "The openess of the network.")
+    parser.add_argument("--description",
+                        help = "The description of this network")
+
     parser.add_argument("--store-local-descriptor-path",
                         help = "The path where the descriptor will be save.")
     parser.add_argument("--force",
