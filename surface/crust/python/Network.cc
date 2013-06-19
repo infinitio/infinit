f0e08d03d2965561a24fbb05b4eea7752432c0c8
Antony MECHIN <antony.mechin@infinit.io>
2013-06-19 22:10:25 +0200

    surface.crust: Update wrapper.

--- surface/crust/python/Network.cc
+++ surface/crust/python/Network.cc
@@ -1,9 +1,54 @@
 #include <wrappers/boost/python.hh>
+
 #include <surface/crust/Network.hh>
+#include <surface/crust/User.hh>
+
+/*--------.
+| Network |
+`--------*/
+
+static
+Network*
+_Network_new(std::string const& identity_path,
+             std::string const& identity_passphrase,
+             std::string const& model,
+             std::string const& openness,
+             std::string const& policy,
+             std::string const& description)
+{
+  // XXX: For the moment, I choosed to remove the Authority, taking meta by
+  // default. This could be improved.
+  return new Network(boost::filesystem::path(identity_path),
+                     identity_passphrase,
+                     hole::Model(model),
+                     hole::openness_from_name(openness),
+                     horizon::policy_from_name(policy),
+                     description);
+}
+
+static
+Network*
+_Network_new_from_path(std::string const& path)
+{
+  return new Network(boost::filesystem::path(path));
+}
+
+static
+Network*
+_Network_new_from_home(std::string const& network_name,
+                       std::string const& owner_name,
+                       std::string const& infinit_home)
+{
+  // XXX: For the moment, I choosed to remove the Authority, taking meta by
+  // default. This could be improved.
+  return new Network(network_name,
+                     owner_name,
+                     boost::filesystem::path(infinit_home));
+}
 
 static
 boost::python::object
-_local_list(std::string const& path)
+_Network_local_list(std::string const& path)
 {
   boost::python::list networks;
 
@@ -15,9 +60,9 @@ _local_list(std::string const& path)
 
 static
 boost::python::object
-_remote_list(std::string const& host,
-             uint16_t port,
-             std::string const& token_path)
+_Network_remote_list(std::string const& host,
+                     uint16_t port,
+                     std::string const& token_path)
 {
   boost::python::list networks;
 
@@ -31,11 +76,11 @@ _remote_list(std::string const& host,
 
 static
 std::string
-_lookup(std::string const& owner_handle,
-        std::string const& network_name,
-        std::string const& host,
-        uint16_t port,
-        std::string const& token_path)
+_Network_lookup(std::string const& owner_handle,
+                std::string const& network_name,
+                std::string const& host,
+                uint16_t port,
+                std::string const& token_path)
 {
   return Network::lookup(owner_handle,
                          network_name,
@@ -46,77 +91,125 @@ _lookup(std::string const& owner_handle,
 
 static
 uint16_t
-_mount(Network* net,
-       std::string const& path,
-       bool run)
+_Network_mount(Network* net,
+               std::string const& path,
+               bool run)
 {
   return net->mount(boost::filesystem::path(path), run);
 }
 
 static
 void
-_store(Network* net,
-       std::string const& path,
-       bool force)
+_Network_store(Network* net,
+               std::string const& path,
+               bool force)
 {
   net->store(boost::filesystem::path(path), force);
 }
 
 static
 void
-_erase(Network* net,
-       std::string const& path)
+_Network_erase(Network* net,
+               std::string const& path)
 {
   net->erase(boost::filesystem::path(path));
 }
 
 static
 void
-_install(Network* net,
-         std::string const& path)
+_Network_install(Network* net,
+                 std::string const& network_name,
+                 std::string const& administrator_name,
+                 std::string const& infinit_home_path)
 {
-  net->install(boost::filesystem::path(path));
+  net->install(network_name,
+               administrator_name,
+               boost::filesystem::path(infinit_home_path));
 }
 
 static
 void
-_uninstall(Network* net,
-           std::string const& path)
+_Network_uninstall(Network* net,
+                   std::string const& network_name,
+                   std::string const& administrator_name,
+                   std::string const& infinit_home_path)
 {
-  net->uninstall(boost::filesystem::path(path));
+  net->uninstall(network_name,
+                 administrator_name,
+                 boost::filesystem::path(infinit_home_path));
+}
+
+/*-----.
+| User |
+`-----*/
+
+static
+User*
+_User_new_from_path(std::string const& path)
+{
+  return new User(boost::filesystem::path(path));
 }
 
 static
-Network*
-_new_network_from_path(std::string const& path)
+void
+_User_store(User* user,
+            std::string const& path,
+            bool force)
 {
-  return new Network(boost::filesystem::path(path));
+  user->store(boost::filesystem::path(path), force);
 }
 
 static
-Network*
-_new_network(std::string const& name,
-             std::string const& identity_path,
-             std::string const& identity_passphrase,
-             std::string const& model,
-             std::string const& openness,
-             std::string const& policy)
+void
+_User_erase(User* user,
+            std::string const& path)
 {
-  // XXX: For the moment, I choosed to remove the Authority, taking meta by
-  // default. This could be improved.
-  return new Network(name,
-                     boost::filesystem::path(identity_path),
-                     identity_passphrase,
-                     hole::Model(model),
-                     hole::openness_from_name(openness),
-                     horizon::policy_from_name(policy));
+  user->erase(boost::filesystem::path(path));
 }
 
+
+static
+void
+_User_install(User* user,
+              std::string const& name,
+              std::string const& infinit_home_path)
+{
+  user->install(name,
+                boost::filesystem::path(infinit_home_path));
+}
+
+static
+void
+_User_uninstall(User* user,
+                std::string const& name,
+                std::string const& infinit_home_path)
+{
+  user->uninstall(name,
+                  boost::filesystem::path(infinit_home_path));
+
+}
+
+//
+// ---------- Python export ----------------------------------------------------
+//
 BOOST_PYTHON_MODULE(_crust)
 {
   namespace py = boost::python;
   typedef py::return_value_policy<py::return_by_value> by_value;
   typedef py::return_value_policy<py::copy_const_reference> by_copy;
+
+  /*---.
+  | ID |
+  `---*/
+
+  py::class_<infinit::Identifier>("ID",
+                                  py::init<size_t>())
+    .def(py::init<std::string const&>())
+  ;
+
+  /*--------.
+  | Network |
+  `--------*/
 
   py::enum_<plasma::meta::Client::DescriptorList>("descriptor_list")
     .value("ALL", plasma::meta::Client::DescriptorList::all)
@@ -124,35 +217,35 @@ BOOST_PYTHON_MODULE(_crust)
     .value("OTHER", plasma::meta::Client::DescriptorList::other)
   ;
 
-  py::class_<ID>("ID",
-                 py::init<size_t>())
-    .def(py::init<std::string const&>())
-  ;
-
   py::class_<Network, boost::noncopyable>(
     "_Network",
     // Constructor with id, meta host, port and token.
-    py::init<ID const&, std::string const&, int16_t, std::string const&>())
+    py::init<infinit::Identifier const&,
+             std::string const&,
+             int16_t,
+             std::string const&>())
     // Constructor with id, meta host and port.
-    .def(py::init<ID const&, std::string const&, int16_t>())
+    .def(py::init<infinit::Identifier const&, std::string const&, int16_t>())
     // Constructor with id and meta host.
-    .def(py::init<ID const&, std::string const&>())
+    .def(py::init<infinit::Identifier const&, std::string const&>())
     // Constructor with id and meta host.
-    .def(py::init<ID const&>())
+    .def(py::init<infinit::Identifier const&>())
     // Constructor with name, user, passphrase, model, policy, opennes.
-    .def("__init__", boost::python::make_constructor(&_new_network))
+    .def("__init__", boost::python::make_constructor(&_Network_new))
     // Constructor with descritpor path.
-    .def("__init__", boost::python::make_constructor(&_new_network_from_path))
+    .def("__init__", boost::python::make_constructor(&_Network_new_from_path))
+    // Constructor with name, owner and home.
+    .def("__init__", boost::python::make_constructor(&_Network_new_from_home))
     // Store the descriptor to the given path.
-    .def("_store", py::make_function(&_store))
+    .def("_store", py::make_function(&_Network_store))
     // Delete the given descriptor. (rm descriptor).
-    .def("_erase", py::make_function(&_erase))
+    .def("_erase", py::make_function(&_Network_erase))
     // Create the directory containing the network shelter.
-    .def("_install", py::make_function(&_install))
+    .def("_install", py::make_function(&_Network_install))
     // Delete the directory containing the network shelter (rm -r networkpath).
-    .def("_uninstall", py::make_function(&_uninstall))
+    .def("_uninstall", py::make_function(&_Network_uninstall))
     // Prepare the mount point and launch infinit if asked.
-    .def("_mount", py::make_function(&_mount))
+    .def("_mount", py::make_function(&_Network_mount))
     // Unmount infinit.
     .def("_unmount", &Network::unmount)
     // Publish the descriptor to the remote.
@@ -160,25 +253,58 @@ BOOST_PYTHON_MODULE(_crust)
     // Unpublish the descriptor from the remote.
     .def("_unpublish", &Network::unpublish)
 
-    .add_property("identifier", py::make_function(&Network::identifier, by_value()))
-    .add_property("administrator_K", py::make_function(&Network::administrator_K, by_value()))
-    .add_property("model", py::make_function(&Network::model, by_value()))
-    .add_property("everybody_identity", py::make_function(&Network::everybody_identity, by_value()))
-    .add_property("history", py::make_function(&Network::history, by_value()))
-    .add_property("extent", py::make_function(&Network::extent, by_value()))
+    .add_property("identifier",
+                  py::make_function(&Network::identifier, by_value()))
+    .add_property("administrator_K",
+                  py::make_function(&Network::administrator_K, by_value()))
+    .add_property("model",
+                  py::make_function(&Network::model, by_value()))
+    .add_property("everybody_identity",
+                  py::make_function(&Network::everybody_identity, by_value()))
+    .add_property("history",
+                  py::make_function(&Network::history, by_value()))
+    .add_property("extent",
+                  py::make_function(&Network::extent, by_value()))
 
-    .add_property("description", py::make_function(&Network::description, by_value()))
-    .add_property("openness", py::make_function(&Network::openness, by_value()))
-    .add_property("policy", py::make_function(&Network::policy, by_value()))
-    .add_property("version", py::make_function(&Network::version, by_value()))
+    .add_property("description",
+                  py::make_function(&Network::description, by_value()))
+    .add_property("openness",
+                  py::make_function(&Network::openness, by_value()))
+    .add_property("policy",
+                  py::make_function(&Network::policy, by_value()))
+    .add_property("version",
+                  py::make_function(&Network::version, by_value()))
   ;
   // List local descriptor from a given path.
-  py::def("_local_list", py::make_function(&_local_list));
+  py::def("_Network_local_list", py::make_function(&_Network_local_list));
   // List the descriptor stored on the remote.
-  py::def("_remote_list", py::make_function(&_remote_list));
+  py::def("_Network_remote_list", py::make_function(&_Network_remote_list));
   // Lookup descriptor id from owner handle and network name.
-  py::def("_lookup", py::make_function(&_lookup));
+  py::def("_Network_lookup", py::make_function(&_Network_lookup));
+
+  /*-----.
+  | User |
+  `-----*/
+  py::class_<User, boost::noncopyable>(
+    "_User",
+    // Constructor with name and password.
+    py::init<std::string const&, std::string const&>())
+    // Constructor with descritpor path.
+    .def("__init__", boost::python::make_constructor(&_User_new_from_path))
+    // Store the identity to the given path.
+    .def("_store", py::make_function(&_User_store))
+    // Delete the given identity. (rm identity).
+    .def("_erase", py::make_function(&_User_erase))
+    // Create the directory containing the user data.
+    .def("_install", py::make_function(&_User_install))
+    // Delete the directory containing the user data including his networks.
+    .def("_uninstall", py::make_function(&_User_uninstall))
+
+    .add_property("identifier",
+                  py::make_function(&User::identifier, by_value()))
+    .add_property("description",
+                  py::make_function(&User::description, by_value()))
+  ;
 
 
-  //py::def("validate", &Network::validate);
 }
