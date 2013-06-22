85481db0149e232d3fe6e52837128619cca36f3b
Julien Quintard <julien.quintard@infinit.io>
2013-06-22 17:13:50 +0200

    infinit: Add an all.hh file which includes everything.
    This file should be used with care however.

---
+++ infinit/infinit/sources/infinit/all.hh
@@ -0,0 +1,12 @@
+#ifndef INFINIT_ALL_HH
+# define INFINIT_ALL_HH
+
+# include <infinit/Authority.hh>
+# include <infinit/Certificate.hh>
+# include <infinit/Descriptor.hh>
+# include <infinit/Exception.hh>
+# include <infinit/Identifier.hh>
+# include <infinit/Identity.hh>
+# include <infinit/infinit.hh>
+
+#endif
