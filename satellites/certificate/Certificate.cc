c35112015a163280bea99fd07b48afda5a01d680
Julien Quintard <julien.quintard@infinit.io>
2013-06-25 08:31:49 +0200

    satellites: Provide an example of how to use the Certificate class.

---
+++ satellites/certificate/Certificate.cc
@@ -0,0 +1,75 @@
+#include <elle/serialize/extract.hh>
+
+#include <satellites/satellite.hh>
+
+#include <infinit/all.hh>
+
+#include <lune/Lune.hh>
+
+#include <chrono>
+
+namespace satellite
+{
+  void
+  Certificate(elle::Natural32 argc,
+              elle::Character* argv[])
+  {
+    if (lune::Lune::Initialize() == elle::Status::Error)
+      throw elle::Exception("unable to initialize Lune");
+
+    // Example for Antony: infinit-certificate-create
+    {
+      elle::String issuer_path("XXX");
+      elle::String issuer_passphrase("XXX");
+
+      elle::String subject_path("XXX");
+
+      elle::String certificate_path("XXX");
+
+      elle::String description("XXX");
+
+      infinit::certificate::Permissions permissions =
+        infinit::certificate::permissions::none; // XXX
+
+      std::chrono::minutes minutes_in_the_past(2 * 365 * 24 * 60);
+      std::chrono::minutes minutes_in_the_future(3 * 365 * 24 * 60);
+
+      // ---
+
+      infinit::Authority issuer(elle::serialize::from_file(issuer_path));
+      // XXX validate
+      cryptography::PrivateKey issuer_k = issuer.decrypt(issuer_passphrase);
+
+      infinit::Authority subject(elle::serialize::from_file(subject_path));
+      // XXX validate
+
+      auto now = std::chrono::system_clock::now();
+
+      std::chrono::system_clock::time_point valid_from =
+        now + minutes_in_the_past;
+      std::chrono::system_clock::time_point valid_until =
+        now + minutes_in_the_future;
+
+      infinit::Certificate certificate(issuer.K(),
+                                       subject.K(),
+                                       description,
+                                       permissions,
+                                       valid_from,
+                                       valid_until,
+                                       issuer_k);
+
+      elle::serialize::to_file(certificate_path) << certificate;
+    }
+
+    if (lune::Lune::Clean() == elle::Status::Error)
+      throw elle::Exception("unable to clean Lune");
+  }
+}
+
+int                     main(int                                argc,
+                             char**                             argv)
+{
+  return satellite_main("8certificate", [&] {
+                          satellite::Certificate(argc, argv);
+                        });
+}
