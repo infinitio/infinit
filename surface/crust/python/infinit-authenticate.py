5dbabdb80c4660ed34497edb3ceeb9ca46447969
Antony MECHIN <antony.mechin@infinit.io>
2013-06-25 19:12:28 +0200

    surface.crust.python: Add authentication script.py

---
+++ surface/crust/python/infinit-authenticate.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import infinit_utils
+def login(user_name, home, host, port):
+    from getpass import getpass
+    password = getpass("Identity password: ")
+
+    from pycrust import User
+
+    user = User(user_name, home, True)
+    return User.store_token(user.login(password, host, port), user_name, home)
+
+def main(args):
+    home = args.infinit_home or getenv("INFINIT_HOME")
+    if not home:
+        raise Exception("You must provide a home directory, with --infinit-home or env INFINIT_HOME")
+    print(login(args.user_name, home, args.meta_host, args.meta_port))
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = infinit_utils.RemoteParser(require_token = False)
+
+    parser.add_argument("--infinit-home",
+                        help = "XXX: The path to your infinit home directory")
+    parser.add_argument("--user-name",
+                        help = "XXX: WILL BE REMOVE, IT'S YOU, IT'S YOUR NAME")
+
+    parser.add_argument("META_USER_NAME",
+                        help = "The name meta will reference you.")
+
+    # Parse arguments and handle errors.
+    infinit_utils.run(parser, main)
