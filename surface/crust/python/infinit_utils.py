8268437c47ec17f24a65083d3728133bd70a795d
Antony MECHIN <antony.mechin@infinit.io>
2013-06-25 19:09:57 +0200

    surface.crust.python: Update utils.

--- surface/crust/python/infinit_utils.py
+++ surface/crust/python/infinit_utils.py
@@ -21,34 +21,66 @@ class LocalParser(argparse.ArgumentParser):
         setattr(args, "infinit_home", home)
         return args
 
-class RemoteParser(argparse.ArgumentParser):
+class AuthorityParser(argparse.ArgumentParser):
     def __init__(self):
         super().__init__()
+
+        self.add_argument("--authority-host",
+                          help = "XXX: The identity host")
+        self.add_argument("--authority-port",
+                          help = "XXX: The identity port")
+    def parse_args(self):
+        args = super().parse_args()
+        from os import getenv
+
+        authority_host = args.authority_host or getenv("INFINIT_AUTHORITY_HOST")
+        if authority_host is None:
+            raise Exception("You neither provided --authority-host nor exported INFINIT_AUTHORITY_HOST.")
+        setattr(args, "authority_host", authority_host)
+
+        authority_port = args.authority_port or getenv("INFINIT_AUTHORITY_PORT")
+        if authority_port is None:
+            raise Exception("You neither provided --authority-port nor exported INFINIT_AUTHORITY_PORT.")
+        setattr(args, "authority_port", int(authority_port))
+
+        return args
+
+class RemoteParser(argparse.ArgumentParser):
+    def __init__(self, require_token = True):
+        super().__init__()
+        self.require_token = require_token
         self.add_argument("--meta-host",
                           help = "XXX: The host. You can also export INFINIT_META_HOST.")
         self.add_argument("--meta-port",
                           type = int,
                           help = "XXX: The port. You can also export INFINIT_META_PORT.")
-        self.add_argument("--meta-token-path",
-                          help = "XXX: The token path. You can also export INFINIT_META_TOKEN_PATH.")
+
+        if self.require_token:
+            self.add_argument("--meta-token-path",
+                              help = "XXX: The token path. You can also export INFINIT_META_TOKEN_PATH.")
 
     def parse_args(self):
         args = super().parse_args()
-        from os import getenv
-        meta_host = args.meta_host or getenv("INFINIT_META_HOST")
-        meta_port = args.meta_port or getenv("INFINIT_META_PORT")
-        meta_token_path = args.meta_token_path or getenv("INFINIT_META_TOKEN_PATH")
+        from os import getenv, path
 
+        meta_host = args.meta_host or getenv("INFINIT_META_HOST")
         if meta_host is None:
             raise Exception("You neither provided --meta-host nor exported INFINIT_META_HOST.")
+        setattr(args, "meta_host", meta_host)
+
+        meta_port = args.meta_port or getenv("INFINIT_META_PORT")
         if meta_port is None:
             raise Exception("You neither provided --meta-port nor exported INFINIT_META_PORT.")
-        if meta_token_path is None:
-            raise Exception("You neither provided --meta-token-path nor exported INFINIT_META_TOKEN_PATH.")
-
-        setattr(args, "meta_host", meta_host)
         setattr(args, "meta_port", int(meta_port))
-        setattr(args, "meta_token_path", meta_token_path)
+
+        if self.require_token:
+            meta_token_path = args.meta_token_path or getenv("INFINIT_META_TOKEN_PATH")
+            if meta_token_path is None:
+                raise Exception("You neither provided --meta-token-path nor exported INFINIT_META_TOKEN_PATH.")
+            if not path.exists(meta_token_path):
+                raise Exception("meta-token-path at %s doesn't exist" % meta_token_path)
+            setattr(args, "meta_token_path", meta_token_path)
+
         return args
 
 def format_string(string):
