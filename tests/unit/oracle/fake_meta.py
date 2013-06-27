8330bcba102cbaa3f2fed7c158581a2c7f77b1d6
mefyl <quentin.hocquet@infinit.io>
2013-06-27 11:20:31 +0200

    oracle.disciples.tests: Enable fake_meta to listen on a random port and write a port file.

--- tests/unit/oracle/fake_meta.py
+++ tests/unit/oracle/fake_meta.py
@@ -1,12 +1,15 @@
-import web
+import argparse
 import json
-        
+import os
+import sys
+import web
+
 urls = (
     '/(.*)', 'hello'
 )
 app = web.application(urls, globals())
 
-class hello:        
+class hello:
     def GET(self, name):
         if name == "self" or name == "user/login":
             return json.dumps({'success': True, "_id": "id", 'token':
@@ -16,4 +19,22 @@ class hello:
         return self.GET(name)
 
 if __name__ == "__main__":
-    app.run()
+    desc = 'Meta server that authenticates everything.'
+    parser = argparse.ArgumentParser(description = desc)
+    parser.add_argument('--port-file',
+                        type = str,
+                        default = None,
+                        help = 'file to write the listening port to')
+    args = parser.parse_args()
+    try:
+        from wsgiref.simple_server import make_server
+        httpd = make_server('', 0, app.wsgifunc())
+        if args.port_file is not None:
+            with open(args.port_file, 'w') as f:
+                print >> f, httpd.server_port
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        if args.port_file is not None:
+            os.remove(args.port_file)
