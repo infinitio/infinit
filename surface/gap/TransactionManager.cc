ab58a63432c25d021ccb4ff5cbc7e1de75fba258
Julien Quintard <julien.quintard@infinit.io>
2013-06-27 11:42:18 +0200

    surface.gap.TransactionManager: Better handle receiving a transaction notification with an empty {sender/recipient}_device_id should the transaction not be accepted yet.

--- surface/gap/TransactionManager.cc
+++ surface/gap/TransactionManager.cc
@@ -530,14 +530,13 @@ namespace surface
       {
         if (tr.sender_device_id != this->_device.id)
         {
-          // ELLE_ASSERT(
-          //     false,
-          //     "got a transaction tr that does not involve my device: %s",
-          //     tr);
-          ELLE_WARN("XXX Should be an assert: got device unrelated tr");
-          return;
+          throw Exception(
+            device_not_valid,
+            elle::sprintf("received notification regarding a created "
+                          "transaction which does not involve this "
+                          "device: %s", tr));
         }
-        if (tr.status == plasma::TransactionStatus::created)
+        else if (tr.status == plasma::TransactionStatus::created)
         {
           ELLE_DEBUG("sender prepare upload for %s", tr)
             this->_prepare_upload(tr);
@@ -560,16 +559,16 @@ namespace surface
       {
         if (tr.recipient_device_id != this->_device.id)
         {
-          // ELLE_ASSERT(
-          //     false,
-          //     "got a transaction tr that does not involve my device: %s",
-          //     tr);
-          ELLE_WARN("XXX Should be an assert: got device unrelated tr");
-          return;
+          if (tr.accepted)
+            throw Exception(
+              device_not_valid,
+              elle::sprintf("received notification regarding a received "
+                            "transaction which does not involve this "
+                            "device: %s", tr));
         }
-        if (tr.status == plasma::TransactionStatus::started &&
-            tr.accepted &&
-            this->_user_manager.device_status(tr.sender_id,
+        else if (tr.status == plasma::TransactionStatus::started &&
+                 tr.accepted &&
+                 this->_user_manager.device_status(tr.sender_id,
                                               tr.sender_device_id))
         {
           ELLE_DEBUG("recipient start download for %s", tr)
