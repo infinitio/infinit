029aee41740153c5f4def5701b7287130583c290
mefyl <quentin.hocquet@infinit.io>
2013-06-29 13:11:10 +0200

    etoile.Etoile: Add first unit test draft.

---
+++ tests/unit/etoile/Etoile.cc
@@ -0,0 +1,47 @@
+#define BOOST_TEST_MODULE Etoile
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+#include <cryptography/KeyPair.hh>
+
+#include <reactor/network/Protocol.hh>
+#include <reactor/scheduler.hh>
+
+#include <hole/storage/Memory.hh>
+#include <hole/implementations/slug/Slug.hh>
+
+#include <etoile/Etoile.hh>
+
+infinit::cryptography::KeyPair authority_keys =
+  infinit::cryptography::KeyPair::generate(
+  infinit::cryptography::Cryptosystem::rsa, 1024);
+elle::Authority authority(authority_keys);
+
+infinit::cryptography::KeyPair keys =
+  infinit::cryptography::KeyPair::generate(
+    infinit::cryptography::Cryptosystem::rsa, 1024);
+
+static
+void
+test()
+{
+  nucleus::proton::Network network("test network");
+  hole::storage::Memory storage(network);
+  elle::Passport passport("passport", "host", keys.K(), authority);
+
+  std::unique_ptr<hole::Hole> slug(
+    new hole::implementations::slug::Slug(storage,
+                                          passport,
+                                          authority,
+                                          reactor::network::Protocol::tcp));
+
+  etoile::Etoile etoile(slug.get(), false);
+}
+
+BOOST_AUTO_TEST_CASE(test_etoile)
+{
+  reactor::Scheduler sched;
+  reactor::Thread t(sched, "main", [&] { test(); });
+  sched.run();
+}
