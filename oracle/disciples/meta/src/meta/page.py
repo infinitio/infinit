14a866c537e6b99d5ee6b0219d2c1cf2d2594f42
Christopher Crone <christopher.crone@infinit.io>
2013-07-01 15:09:39 +0200

    oracle.disciples.meta: Remove unused code.

--- oracle/disciples/meta/src/meta/page.py
+++ oracle/disciples/meta/src/meta/page.py
@@ -24,11 +24,6 @@ def replacer(match):
     field = match.group(2)
     return match.group(1) + "'" + field + "'" + match.group(3)
 
-def USER_STATUS_M(name, value):
-    globals()[name.upper()] = value
-
-filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), 'user_status.hh.inc'))
-
 configfile = open(filepath, 'r')
 for line in configfile:
     eval(_macro_matcher.sub(replacer, line))
