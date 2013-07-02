43d59e45cb5e6b971554777766d8e02ceac89a68
Raphaël Londeix <raphael.londeix@infinit.io>
2013-07-02 18:34:04 +0200

    oracle.meta: Consider a transaction new until accepted, canceled, or failed.

--- oracle/disciples/meta/src/meta/resources/notification.py
+++ oracle/disciples/meta/src/meta/resources/notification.py
@@ -5,6 +5,7 @@ from meta.page import Page
 from meta import database
 from meta import error
 from meta import regexp
+from meta import notifier
 
 import meta.mail
 
@@ -12,8 +13,19 @@ import re
 
 import metalib
 
+def TRANSACTION_STATUS(name, value):
+    globals()[name.upper()] = value
+    _status_to_string[value] = str(name)
 
-class Get(Page):
+filepath = os.path.abspath(
+  os.path.join(os.path.dirname(__file__), 'transaction_status.hh.inc')
+)
+
+configfile = open(filepath, 'r')
+for line in configfile:
+    eval(_macro_matcher.sub(replacer, line))
+
+class All(Page):
     """
     POST {
               'count': the number of notif you want to pull.
@@ -25,49 +37,36 @@ class Get(Page):
            }
     """
 
-    __pattern__ = "/notification/get"
+    __pattern__ = "/notifications"
 
     def POST(self):
         if not self.user:
             return self.error(error.NOT_LOGGED_IN)
 
-        _user = database.users().find_one(database.ObjectId(self.user['_id']))
+        notifications = self.user['notifications']
+        count = self.data.get('count', 10)
+        offset = min(self.data.get('offset', 0), len(notifications) - 1)
 
-        if not _user:
-          return self.error(error.UNKNOWN_USER)
-
-        #XXX: limit
-        try:
-            limit = self.data['limit']
-        except Exception as e:
-            pass
-
-        try:
-            count = self.data['count']
-            offset = self.data['offset']
-        except Exception as e:
-            count = limit
-            offset = 0
-
-        notifs = _user['notifications'][offset:]
+        notifs = notifications[offset:]
         old_notifs = []
-        if (len(notifs) < count):
-            old_notifs = _user['old_notifications'][:(count - len(notifs))]
-            old_notifs.reverse()
+        if len(notifs) < count:
+            old_notifs = self.user['old_notifications'][:(count - len(notifs))]
+            old_notifs.reverse() # XXX remove
 
         return self.success({
             'notifs' : notifs,
             'old_notifs': old_notifs,
         })
 
+# XXX backward compat
+class Get(All):
+    __pattern__ = "/notification/get"
+
 class Read(Page):
     """
-    POST {
-
-         }
-         -> {
-            }
-
+    GET -> {
+        'success': True
+    }
     """
 
     __pattern__ = "/notification/read"
@@ -76,10 +75,18 @@ class Read(Page):
         if not self.user:
             return self.error(error.NOT_LOGGED_IN)
 
-        self._user['notifications'].reverse()
-        self._user['old_notifications'] = self._user['notifications'] + self._user['old_notifications']
+        self._user['notifications'].reverse() # XXX remove
+        old = []
+        new = []
+        for n in self._user['notifications']:
+            if n['notification_type'] == notifier.TRANSACTION and \
+               n['status'] and in [CANCELLED, FAILED, FINISHED]:
+                new.append(n)
+            else:
+                old.append(n)
 
-        self._user['notifications'] = []
+        self._user['old_notifications'].insert(0, old)
+        self._user['notifications'] = new
 
         database.users().save(self._user)
 
