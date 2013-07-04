601f67a0aba437eacb36b88b6852a891fcce11cd
Raphaël Londeix <raphael.londeix@infinit.io>
2013-07-04 17:28:19 +0200

    Try to rename walrus

--- tests/unit/walrus.py
+++ tests/unit/walrus.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
-from __future__ import print_function
-from twisted.internet import protocol, reactor
-from twisted.protocols import basic
-from twisted.internet.endpoints import TCP4ClientEndpoint
-from twisted.python import log
+import socket
 import subprocess
 import socket
 import argparse
@@ -12,20 +8,7 @@ import sys
 import os
 import tempfile
 
-THIS_FILE = os.path.realpath(__file__)
-
-arguments = argparse.ArgumentParser()
-
-arguments.add_argument("--port", type=int, default=0)
-arguments.add_argument("--control-port", type=int, default=0)
-arguments.add_argument("--trophonius-host", type=str, default="development.infinit.io")
-arguments.add_argument("--trophonius-port", type=int, default=23456)
-arguments.add_argument("--log-file", default=sys.stdout)
-arguments.add_argument("--runtime-dir", default="")
-
-args = arguments.parse_args()
-
-trophonius = TCP4ClientEndpoint(reactor, args.trophonius_host, args.trophonius_port)
+walrus_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "walrus.py")
 
 class Walrus:
     def __init__(self, port=0, control_port=0, trophonius_host="127.0.0.1", trophonius_port=0):
@@ -34,10 +17,11 @@ class Walrus:
         self.thost = trophonius_host
         self.tport = trophonius_port
         self.__directory = tempfile.mkdtemp()
+        self.control = None
 
     def __enter__(self):
         import time
-        args = [THIS_FILE,
+        args = [walrus_path,
             "--port", str(self.__port),
             "--control-port", str(self.__control_port),
             "--trophonius-host", self.thost,
@@ -47,6 +31,7 @@ class Walrus:
         print(args)
         self.instance = subprocess.Popen(args)
         time.sleep(2)
+        self.control = socket.create_connection(("127.0.0.1", self.control_port()))
         return self
 
     def _read_ports(self):
@@ -67,98 +52,8 @@ class Walrus:
             self._read_ports()
         return self.__control_port
 
+    def pause(self):
+        self.control.send(b"pause\n")
+
     def __exit__(self, exception_type, exception, traceback):
         self.instance.terminate()
-
-class Echo(protocol.Protocol):
-    def __init__(self, wire):
-        self.wire = wire
-
-    def dataReceived(self, data):
-        self.wire.dataFromPeer(data)
-
-    def connectionLost(self, reason):
-        log.msg(reason.getErrorMessage())
-        self.wire.transport.abortConnection()
-
-class RemoteEndpointFactory(protocol.Factory):
-    def __init__(self, wire):
-        self.wire = wire
-
-    def buildProtocol(self, addr):
-        return Echo(self.wire)
-
-class Wire(protocol.Protocol):
-    def __init__(self, addr):
-        self.lhs = addr
-        self.rhs = None
-        self.pause = False
-        self.connection = trophonius.connect(RemoteEndpointFactory(self))
-        self.connection.addCallback(self.trophoniusConnectionMade)
-        self.data_cache = []
-
-    def trophoniusConnectionMade(self, proto):
-        self.rhs = proto
-        for data in self.data_cache:
-            print("send", len(data), "from cache");
-            self.rhs.transport.write(data)
-        self.data_cache = None
-
-    def dataFromPeer(self, data):
-        if self.pause is False:
-            self.transport.write(data)
-
-    def dataReceived(self, data):
-        if self.connection.called is False:
-            self.data_cache.append(data)
-        elif self.rhs is not None and self.pause is False:
-            self.rhs.transport.write(data)
-
-    def stop(self):
-        print("pausing", self)
-        self.pause = True
-
-    def connectionLost(self, reason):
-        log.msg(reason.getErrorMessage())
-
-class WireFactory(protocol.Factory):
-    def __init__(self, clist):
-        self.clist = clist
-
-    def buildProtocol(self, addr):
-        print("connection from", addr)
-        wire = Wire(addr)
-        self.clist.append(wire)
-        return wire
-
-class Control(basic.LineReceiver):
-    delimiter = "\n"
-
-    def __init__(self, clist):
-        self.clist = clist
-
-    def lineReceived(self, line):
-        msg = line.strip()
-        if msg == "pause":
-            for c in self.clist:
-                c.stop()
-
-class ControlFactory(protocol.Factory):
-    def __init__(self, clist):
-        self.clist = clist
-
-    def buildProtocol(self, addr):
-        return Control(self.clist)
-
-if __name__ == "__main__":
-    print(THIS_FILE)
-    client_lists = []
-    log.startLogging(sys.stdout)
-    port = reactor.listenTCP(args.port, WireFactory(client_lists))
-    control_port = reactor.listenTCP(args.control_port, ControlFactory(client_lists))
-    if args.runtime_dir:
-        with open(os.path.join(args.runtime_dir, "portfile"), "w+") as portfile:
-                portfile.write("port:{}\n".format(port.getHost().port))
-                portfile.write("control_port:{}\n".format(control_port.getHost().port))
-    print("listenning on", port.getHost(), control_port.getHost())
-    reactor.run()
