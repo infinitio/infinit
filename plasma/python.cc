36a74abd4ec654bdf6a95252fa9a160fafc0af3d
mefyl <quentin.hocquet@infinit.io>
2013-07-04 15:38:23 +0200

    plasma: Pacify -Wmissing-declaration warning.

--- plasma/python.cc
+++ plasma/python.cc
@@ -3,6 +3,12 @@
 void export_trophonius();
 void export_meta();
 
+// Pacify -Wmissing-declaration
+extern "C"
+{
+  PyObject* PyInit_plasma();
+}
+
 BOOST_PYTHON_MODULE(plasma)
 {
   export_meta();
