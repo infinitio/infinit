944df598469b4db2bf180aae820de2c58a9c4a21
Fabien `chipot' PICHOT <pichot.fabien@infinit.io>
2013-07-05 11:18:18 +0200

    test.utils: Fix forgotten prefix.

--- tests/functional/servers/utils.py
+++ tests/functional/servers/utils.py
@@ -19,15 +19,15 @@ class Servers:
         self.tropho.__enter__()
         return self.meta, self.tropho
 
-    def __exit__(self, exception, *args):
-        self.tropho.__exit__(exception, *args)
-        self.meta.__exit__(exception, *args)
-        if exception is not None:
-            print('======== Trophonius stdout:\n' + tropho.stdout,
+    def __exit__(self, exception_type, exception, *args):
+        self.tropho.__exit__(exception_type, exception, *args)
+        self.meta.__exit__(exception_type, exception, *args)
+        if exception is not None and exception_type is not AssertionError:
+            print('======== Trophonius stdout:\n' + self.tropho.stdout,
                   file = sys.stderr)
-            print('======== Trophonius stderr:\n' + tropho.stderr,
+            print('======== Trophonius stderr:\n' + self.tropho.stderr,
                   file = sys.stderr)
-            print('======== Meta stdout:\n' + meta.stdout,
+            print('======== Meta stdout:\n' + self.meta.stdout,
                   file = sys.stderr)
-            print('======== Meta stderr:\n' + meta.stderr,
+            print('======== Meta stderr:\n' + self.meta.stderr,
                   file = sys.stderr)
