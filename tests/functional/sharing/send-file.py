6c5419ba72ac31738e0088e31c7e0616a7fe3436
Antony MECHIN <antony.mechin@infinit.io>
2013-07-10 17:59:18 +0200

    sharing: Improve file sending test.

--- tests/functional/sharing/send-file.py
+++ tests/functional/sharing/send-file.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 
-import apertus
 import gap
-import meta
-import pythia
-import trophonius
 import random
 import hashlib
 
@@ -36,9 +32,17 @@ class RandomTempFile:
     def __init__(self,
                  size = 0,
                  random_ratio : "How many bytes are randomized" = 0.1,
+                 in_directory = False,
                  **kw):
         self.size = size
         self.random_ratio = random_ratio
+        if in_directory: # Update the destination.
+            destination = os.path.join(tempfile.gettempdir(), str(size))
+            import shutil
+            if os.path.isdir(destination):
+                shutil.rmtree(destination)
+            os.makedirs(destination)
+            kw['dir'] = destination
         self.file = tempfile.NamedTemporaryFile(**kw)
         if self.size > 0:
             self.file.truncate(self.size)
@@ -80,6 +84,7 @@ class Transaction:
         assert isinstance(id, str)
         self.id = id
         self.state = state
+        self.localy_accepted = False
 
     @property
     def status(self):
@@ -94,6 +99,16 @@ class Transaction:
     @property
     def progress(self):
         return self.state.transaction_progress(self.id)
+
+    @property
+    def status(self):
+        return self.state.transaction_status(self.id)
+
+    def __str__(self):
+        return "Transaction(%s, %s, localy_accepted: %s)" % (self.id, self.status, self.localy_accepted)
+
+    def __repr__(self):
+        return "Transaction(%s, %s, localy_accepted: %s)" % (self.id, self.status, self.localy_accepted)
 
 class User:
     def __init__(self,
@@ -116,13 +131,15 @@ class User:
         self.output_dir = output_dir
         self.use_temporary = self.output_dir is None
         self.temporary_output_dir = None
+        self.finished = False
+        self.accepted_transactions=set()
 
     def __enter__(self):
         # state setup
         assert self.state == None
         self.state = gap.State(
             "localhost", int(self.meta_server.meta_port),
-            "0.0.0.0", int(trophonius_server.port)
+            "0.0.0.0", int(self.trophonius_server.port)
         )
         self.state.__enter__()
 
@@ -140,6 +157,7 @@ class User:
         else:
             self.state.login(self.email, "password")
 
+        self.id = self.state._id
         # output directory setup
         if self.use_temporary:
             assert self.temporary_output_dir is None
@@ -152,8 +170,7 @@ class User:
 
         self.state.set_output_dir(self.output_dir)
 
-        self.transactions = {}
-        self.id = self.state._id
+        self.transactions = dict()
         self.state.transaction_callback(self._on_transaction)
 
     def __exit__(self, type, value, traceback):
@@ -177,60 +194,76 @@ class User:
                 file = sys.stderr
             )
 
-    def send_file(self, recipient = None, file = None):
-        assert isinstance(file, str)
-        assert isinstance(recipient, User)
-        return self.send_files(recipient = recipient, files = [file])
-
     def send_files(self, recipient = None, files = None):
+        assert isinstance(recipient, User)
+        assert isinstance(files, list)
         self.state.send_files(recipient.id, files)
 
     def _on_transaction(self, transaction_id, status, is_new):
-        self.transactions[transaction_id] = Transaction(
-            self.state,
-            transaction_id
-        )
+        if transaction_id not in self.transactions:
+            self.transactions[transaction_id] = Transaction(
+                self.state,
+                transaction_id
+            )
+        self.on_transaction(transaction_id, status, is_new)
+
+    def on_transaction(self, transaction_id, status, is_new):
+        assert is_new
+        state = self.state
+        assert self.id == state.transaction_recipient_id(transaction_id) or \
+               self.id == state.transaction_sender_id(transaction_id)
+        is_sender = (self.id == self.state.transaction_sender_id(transaction_id))
+        if status == state.TransactionStatus.canceled:
+            print("Transaction canceled")
+            sys.exit(1)
+        elif status == state.TransactionStatus.failed:
+            print("Transaction canceled")
+            sys.exit(1)
+        elif not is_sender and status == state.TransactionStatus.created and \
+             not self.transactions[transaction_id].localy_accepted:
+            self.transactions[transaction_id].localy_accepted = True
+            state.accept_transaction(transaction_id)
+        elif status == state.TransactionStatus.finished:
+            self.finished = True
 
 class TransferReactor:
-    def __init__(self, sender = None, recipient = None, file = None):
+    def __init__(self, sender = None, recipient = None, files = None):
         assert sender is not None
         assert recipient is not None
-        assert file is not None
+        assert files is not None
+        assert isinstance(files, list)
         self.sender = sender
         self.recipient = recipient
-        self.file = file
+        self.files = files
+        self.name = " ".join(self.files)
 
     def run(self, timeout = 300):
         assert len(self.sender.transactions) == 0
         assert len(self.recipient.transactions) == 0
 
-        self.sender.send_file(
-            file = self.file,
-            recipient = self.recipient
-        )
+        expected_files = [
+            os.path.join(self.recipient.output_dir,
+                         os.path.basename(file))
+            for file in self.files]
+
+        print("Sender file", self.files)
+        print("Recipient file", expected_files)
+
+        self.sender.send_files(files = self.files, recipient = self.recipient)
         start = time.time()
-        expected_file = os.path.join(
-            self.recipient.output_dir,
-            os.path.basename(self.file)
-        )
-        print("Sender file", self.file)
-        print("Recipient file", expected_file)
+
         transaction_finished = False
         transaction = None
-        accepted = False
-        while time.time() - start < timeout:
+        while not (sender.finished and recipient.finished) and \
+                time.time() - start < timeout:
+            time.sleep(0.5)
             self.poll()
             time.sleep(0.1)
             if len(self.recipient.transactions):
                 assert len(self.recipient.transactions) == 1
                 transaction_id = list(self.recipient.transactions.keys())[0]
                 transaction = self.recipient.transactions[transaction_id]
-                assert transaction_id in self.sender.transactions
-                if not accepted:
-                    self.recipient.state.accept_transaction(transaction.id)
-                    print('$' * 80)
-                    print("@@@@ ACCEPTED")
-                    accepted = True
+                # ?? assert transaction_id in self.sender.transactions
             if transaction is not None:
                 if transaction.status == "finished":
                     print('$' * 80)
@@ -239,58 +272,59 @@ class TransferReactor:
                     break
                 print('#' * 80, transaction.progress)
 
-        if os.path.exists(expected_file):
-            print('$' * 80)
-            print("@@@@ Found expected file !")
-            if file_sha1(self.file) != file_sha1(expected_file):
-                raise TestFailure("Wrong SHA1")
-            return True
-        raise TestFailure()
+        for file, expected_file in zip(self.files, expected_files):
+            if os.path.exists(expected_file):
+                print('$' * 80)
+                print("@@@@ Found expected file %s!" % expected_file)
+                if file_sha1(file) != file_sha1(expected_file):
+                    raise TestFailure("Wrong SHA1")
+                return True
+        raise TestFailure(self.name)
 
     def poll(self):
         self.sender.state.poll()
         self.recipient.state.poll()
 
 class TestFailure(Exception):
-    pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
 
 if __name__ == '__main__':
     # XXX: For the moment, there is an interdependence between meta and tropho.
     # As long as it stands, we need to have a control port.
     trophonius_control_port = 39074
 
-    with apertus.Apertus() as apertus_server, \
-         meta.Meta(
-             spawn_db = True,
-             trophonius_control_port = trophonius_control_port,
-             apertus_control_port = apertus_server.port
-         ) as meta_server, \
-         trophonius.Trophonius(
-             meta_port = meta_server.meta_port,
-             control_port = trophonius_control_port
-         ) as trophonius_server:
-
+    import utils
+    with utils.Servers() as (meta, trophonius, apertus):
         sender, recipient = (
             User(
-                meta_server = meta_server,
-                trophonius_server = trophonius_server,
+                meta_server = meta,
+                trophonius_server = trophonius,
                 fullname = "sender",
                 email = "sender@infinit.io",
-                register = True,
-            ),
+                register = True
+                ),
             User(
-                meta_server = meta_server,
-                trophonius_server = trophonius_server,
+                meta_server = meta,
+                trophonius_server = trophonius,
                 fullname = "recipient",
                 email = "recipient@infinit.io",
                 register = True,
-            ),
+                ),
         )
-        for size in [40, 4000, 4000000, 400000000]:
-            with RandomTempFile(size = size) as f:
-                with sender, recipient:
-                    TransferReactor(
-                        sender = sender,
-                        recipient = recipient,
-                        file = f.name
-                    ).run(timeout = 30)
+
+        cases = [
+            RandomTempFile(40),
+            RandomTempFile(1000, in_directory = True),
+            RandomTempFile(4000000),
+            RandomTempFile(400000000),
+            [RandomTempFile(30), RandomTempFile(300)],
+        ]
+        for item in cases:
+            files = isinstance(item, list) and [file.name for file in item] or [item.name]
+            with sender, recipient:
+                TransferReactor(
+                    sender = sender,
+                    recipient = recipient,
+                    files = files,
+                ).run(timeout = 30)
