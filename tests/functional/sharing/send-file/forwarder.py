60717e85dd7b66b9ce5034151719ca6f61444d77
Antony MECHIN <antony.mechin@infinit.io>
2013-07-18 12:18:26 +0200

    sharing: Update forwarder test.

--- tests/functional/sharing/send-file/forwarder.py
+++ tests/functional/sharing/send-file/forwarder.py
@@ -9,13 +9,6 @@ import utils
 
 if __name__ == '__main__':
 
-    cases = [
-        [RandomTempFile(4)] * 10,
-        RandomTempFile(400),
-        RandomDirectory(file_count = 512, min_file_size = 10, max_file_size = 1024),
-        [RandomDirectory(file_count = 10, min_file_size = 128, max_file_size = 2048), RandomTempFile(100), RandomTempFile(40000)],
-    ]
-
     import utils
     with utils.Servers() as (meta, trophonius, apertus):
         # Forwarder.
@@ -29,9 +22,9 @@ if __name__ == '__main__':
                                   apertus_server = apertus,
                                   register = True))
 
-        for item in cases:
+        for item in utils.cases():
             files = isinstance(item, list) and [file.name for file in item] or [item.name]
             with sender, recipient:
                 DefaultScenario(sender = sender,
                                 recipient = recipient,
-                                files = files).run(timeout = 1024 * 1024)
+                                files = files).run()
