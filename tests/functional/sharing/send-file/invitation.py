470d87fe6d0cc18ac87aa36295540db080d9fdac
Antony MECHIN <antony.mechin@infinit.io>
2013-07-18 12:18:39 +0200

    sharing: Update invitation test.

--- tests/functional/sharing/send-file/invitation.py
+++ tests/functional/sharing/send-file/invitation.py
@@ -9,13 +9,6 @@ import utils
 
 if __name__ == '__main__':
 
-    cases = [
-        [RandomTempFile(4)] * 10,
-        RandomTempFile(400),
-        RandomDirectory(file_count = 512, min_file_size = 10, max_file_size = 1024),
-        [RandomDirectory(file_count = 10, min_file_size = 128, max_file_size = 2048), RandomTempFile(100), RandomTempFile(40000)],
-    ]
-
     import utils
     with utils.Servers() as (meta, trophonius, apertus):
         # Invitations.
@@ -27,9 +20,9 @@ if __name__ == '__main__':
                                        trophonius_server = trophonius,
                                        apertus_server = apertus))
 
-        for item in cases[0:3]:
+        for item in utils.cases()[0:3]:
             files = isinstance(item, list) and [file.name for file in item] or [item.name]
             with sender, recipient:
                 GhostScenario(sender = sender,
                               recipient = recipient,
-                              files = files).run(timeout = 1024 * 1024)
+                              files = files).run()
