f778b9c690e548941f6335a417bdbe2cb236c756
Antony MECHIN <antony.mechin@infinit.io>
2013-07-24 19:28:54 +0200

    surface.gap.TransactionManager: Remove everything that doesn't concern transaction caching.

--- surface/gap/TransactionManager.hh
+++ surface/gap/TransactionManager.hh
@@ -1,28 +1,18 @@
 #ifndef SURFACE_GAP_TRANSACTIONMANAGER_HH
 # define SURFACE_GAP_TRANSACTIONMANAGER_HH
 
-# include "Device.hh"
-# include "NetworkManager.hh"
 # include "NotificationManager.hh"
-# include "Self.hh"
-# include "Transaction.hh"
-# include "TransactionStateMachine.hh"
-# include "UserManager.hh"
-# include "metrics.hh"
-# include "status.hh"
+# include "usings.hh"
 
-# include <metrics/fwd.hh>
-
-# include <plasma/trophonius/Client.hh>
 # include <plasma/meta/Client.hh>
 
 # include <elle/attribute.hh>
 # include <elle/container/set.hh>
 # include <elle/Printable.hh>
+# include <elle/threading/Monitor.hh>
 
 # include <string>
 # include <unordered_set>
-# include <reactor/scheduler.hh>
 
 namespace surface
 {
@@ -31,80 +21,20 @@ namespace surface
     class TransactionManager:
       public Notifiable
     {
-      /*-----------.
-      | Attributes |
-      `-----------*/
-    private:
       typedef std::function<Self const&()> SelfGetter;
       typedef std::function<Device const&()> DeviceGetter;
-      typedef std::function<void(unsigned int)> UpdateRemainingInvitations;
-
-    private:
-      NetworkManager& _network_manager;
-      ELLE_ATTRIBUTE_R(UserManager&, user_manager);
-      // XXX: meta should be constant everywhere.
-      // But httpclient fire can't be constant.
-      plasma::meta::Client& _meta;
-      metrics::Reporter& _reporter;
-
-      ELLE_ATTRIBUTE(reactor::Scheduler&, scheduler);
-      ELLE_ATTRIBUTE_R(SelfGetter, self);
-      ELLE_ATTRIBUTE(DeviceGetter, device);
-      ELLE_ATTRIBUTE(UpdateRemainingInvitations, update_remaining_invitations);
-      ELLE_ATTRIBUTE_R(std::string, output_dir);
-      ELLE_ATTRIBUTE(TransactionStateMachine, state_machine);
 
       /*-------------.
       | Construction |
       `-------------*/
     public:
-      TransactionManager(
-        reactor::Scheduler& scheduler,
-        NotificationManager& notification_manager,
-        NetworkManager& network_manager,
-        UserManager& user_manager,
-        plasma::meta::Client& meta,
-        metrics::Reporter& reporter,
-        SelfGetter const& self,
-        DeviceGetter const& device,
-        UpdateRemainingInvitations const& update_remaining_invitations);
+      TransactionManager(surface::gap::NotificationManager& notification_manager,
+                         plasma::meta::Client const& meta,
+                         SelfGetter const& self,
+                         DeviceGetter const& device);
 
       virtual
       ~TransactionManager();
-
-      /// @brief Clear the current manager and his sons.
-      void
-      clear();
-
-      /// @brief Set output dir.
-      void
-      output_dir(std::string const& dir);
-
-      /*----------------------.
-      | Sending and recieving |
-      `----------------------*/
-    public:
-      /// @brief Send a file list to a specified user.
-      ///
-      /// Create a network, copy files locally, create transaction.
-      void
-      send_files(std::string const& recipient_id_or_email,
-                 std::unordered_set<std::string> const& files);
-
-    private:
-      /*---------.
-      | Progress |
-      `---------*/
-      struct TransactionProgress;
-      typedef std::unique_ptr<TransactionProgress> TransactionProgressPtr;
-      typedef std::map<std::string, TransactionProgressPtr> TransactionProgressMap;
-      elle::threading::Monitor<TransactionProgressMap> _progresses;
-
-    public:
-      /// @brief Returns a floating number in [0.0f, 1.0f]
-      float
-      progress(std::string const& transaction_id);
-
 
       /*--------.
       | Storage |
@@ -115,9 +45,13 @@ namespace surface
       elle::threading::Monitor<TransactionMapPtr> _transactions;
 
     public:
-      /// @brief Pull transactions from serveur.
+      /// @brief Get the transactions.
       TransactionsMap const&
       all();
+
+      /// @brief Get the transactions.
+      std::vector<std::string>
+      all_ids();
 
       /// @brief Get data from a specific transaction.
       Transaction const&
@@ -127,98 +61,12 @@ namespace surface
       Transaction const&
       sync(std::string const& id);
 
-    private:
-      // Remove processes, states and operations related to a transaction.
-      void
-      _clean_transaction(Transaction const& transaction);
-
-      /*-------------------.
-      | Transaction update |
-      `-------------------*/
-    private:
-      struct State:
-        public elle::Printable
-      {
-        enum
-        {
-          none,     // Unknown transaction.
-          accepting,
-          preparing,
-          running,
-          finished,
-          canceled,
-        } state;
-
-        int tries;
-        std::unordered_set<std::string> files;
-        State():
-          state{none},
-          tries{0}
-        {}
-
-        /*----------.
-        | Printable |
-        `----------*/
-        void
-        print(std::ostream& stream) const
-        {
-          stream << "State::State(";
-          switch (this->state)
-          {
-            case none:
-              stream << "none"; break;
-            case accepting:
-              stream << "accepting"; break;
-            case preparing:
-              stream << "preparing"; break;
-            case running:
-              stream << "running"; break;
-            case finished:
-              stream << "finished"; break;
-            case canceled:
-              stream << "canceled"; break;
-          }
-          stream << ", files: " << files << ")";
-        }
-      };
-      typedef std::map<std::string, State> StateMap;
-      elle::threading::Monitor<StateMap> _states;
-
-    protected:
-      /// @brief Update transaction status.
-      void
-      _update(std::string const& transaction_id,
-              plasma::TransactionStatus status);
-
-    public:
-      void
-      accept_transaction(Transaction const& transaction);
-      void
-      accept_transaction(std::string const& transaction_id);
-
-      void
-      cancel_transaction(std::string const& transaction_id);
-
-    private:
-
-      void
-      _cancel_transaction(Transaction const& transaction);
-
-      void
-      _on_cancel_transaction(Transaction const& transaction);
-
-      void
-      _on_failed_transaction(Transaction const& transaction);
-
-    private:
-      void
-      _prepare_upload(Transaction const& transaction);
-
-      void
-      _start_upload(Transaction const& transaction);
-
-      void
-      _start_download(Transaction const& transaction);
+      /*-----------.
+      | Attributes |
+      `-----------*/
+      ELLE_ATTRIBUTE(plasma::meta::Client const&, meta);
+      ELLE_ATTRIBUTE(SelfGetter, self);
+      ELLE_ATTRIBUTE(DeviceGetter, device);
 
       /*----------.
       | Callbacks |
@@ -227,14 +75,7 @@ namespace surface
       /// @brief Callback when recieving an new transaction.
       void
       _on_transaction(plasma::Transaction const& notif);
-      void
-      _on_user_status(UserStatusNotification const& notif);
     };
-
-    metrics::Metric
-    transaction_metric(Self const& self,
-                       UserManager& user_manager,
-                       Transaction const& tr);
   }
 }
 
