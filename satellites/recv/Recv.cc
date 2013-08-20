f171dae19d6e1bc6b675753e2f012017bc463264
Antony MECHIN <antony.mechin@infinit.io>
2013-08-20 21:51:47 +0200

    satellites.Recv: Remove output.

--- satellites/recv/Recv.cc
+++ satellites/recv/Recv.cc
@@ -110,7 +110,6 @@ int main(int argc, char** argv)
 
         do
         {
-          std::cerr << "a" << std::endl;
           state.poll();
           reactor::Sleep s{sched, boost::posix_time::seconds{1}};
           s.run();
