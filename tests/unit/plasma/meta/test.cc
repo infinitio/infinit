1bc5f874276b92889bad4f674cf0652bd3e27313
mefyl <quentin.hocquet@infinit.io>
2013-08-20 16:52:31 +0200

    plasma.meta.Client: Add concurrent request test.

--- tests/unit/plasma/meta/test.cc
+++ tests/unit/plasma/meta/test.cc
@@ -1,9 +1,11 @@
-#define BOOST_TEST_MODULE meta
 #define BOOST_TEST_DYN_LINK
+
 #include <boost/test/unit_test.hpp>
 
 #include <elle/log.hh>
 
+#include <reactor/Barrier.hh>
+#include <reactor/Scope.hh>
 #include <reactor/network/buffer.hh>
 #include <reactor/network/tcp-server.hh>
 #include <reactor/scheduler.hh>
@@ -27,13 +29,15 @@ wait(reactor::Waitable& w)
   reactor::Scheduler::scheduler()->current()->wait(w);
 }
 
-BOOST_AUTO_TEST_CASE(debug)
+static
+void
+_debug_queries(int count)
 {
-  ELLE_LOG("test debug query");
+  ELLE_LOG("test multiple debug queries");
+
   reactor::Scheduler sched;
 
-  reactor::Semaphore sync_client;
-  reactor::Semaphore sync_server;
+  reactor::Barrier listening;
 
   int port = -1;
   auto serv = [&]
@@ -43,33 +47,66 @@ BOOST_AUTO_TEST_CASE(debug)
     server.listen(0);
     port = server.port();
     ELLE_LOG("listen on port %s", port);
-    sync_client.release(); // Listening
-    std::unique_ptr<reactor::network::TCPSocket> socket(server.accept());
-    ELLE_LOG("connection accepted");
+    listening.open();
 
-    char buf[512];
-    size_t bytes = socket->read_some(
-      reactor::network::Buffer(buf, sizeof(buf)), 1_sec);
-    std::string data(buf, bytes);
-    ELLE_LOG("read: %s", data);
+    reactor::Scope scope;
+    int answered = 0;
+    reactor::Barrier done;
+    for (int i = 0; i < count; ++i)
+    {
+      auto handler = [s = server.accept(), count, &answered, &done]
+      {
+        std::unique_ptr<reactor::network::TCPSocket> socket(s);
+        ELLE_LOG("connection accepted");
 
-    std::string resp("HTTP/1.0 200 OK\n\n{\"success\": true}\n");
-    ELLE_LOG("write: %s", resp);
-    socket->write(reactor::network::Buffer(resp));
+        char buf[512];
+        size_t bytes = socket->read_some(
+          reactor::network::Buffer(buf, sizeof(buf)), 1_sec);
+        std::string data(buf, bytes);
+        ELLE_LOG("read: %s", data);
+
+        std::string resp("HTTP/1.0 200 OK\n\n{\"success\": true}\n");
+        ELLE_LOG("write: %s", resp);
+        socket->write(reactor::network::Buffer(resp));
+        if (++answered == count)
+          done.open();
+      };
+      scope.run_background(elle::sprintf("handler %s", i), handler);
+    }
+    wait(done);
   };
   reactor::Thread s(sched, "server", serv);
 
   auto client = [&]
   {
-    wait(sync_client); // Listening
+    wait(listening); // Listening
     plasma::meta::Client c("127.0.0.1", port);
-    c.debug();
+    ELLE_LOG("send debug request")
+      c.debug();
+    ELLE_LOG("debug request completed");
   };
-  reactor::Thread c(sched, "client", client);
+  for (int i = 0; i < count; ++i)
+    new reactor::Thread(sched, elle::sprintf("client %s", i), client, true);
   sched.run();
 }
 
-BOOST_AUTO_TEST_CASE(timeout)
+static
+void
+debug_query()
+{
+  _debug_queries(1);
+}
+
+static
+void
+debug_queries()
+{
+  _debug_queries(3);
+}
+
+static
+void
+timeout()
 {
   ELLE_LOG("test debug query timeout");
   reactor::Scheduler sched;
@@ -100,4 +137,22 @@ BOOST_AUTO_TEST_CASE(timeout)
   };
   reactor::Thread c(sched, "client", client);
   sched.run();
+}
+
+
+static
+bool
+test_suite()
+{
+  auto& suite = boost::unit_test::framework::master_test_suite();
+  suite.add(BOOST_TEST_CASE(debug_query));
+  suite.add(BOOST_TEST_CASE(debug_queries));
+  suite.add(BOOST_TEST_CASE(timeout));
+  return true;
+}
+
+int
+main(int argc, char** argv)
+{
+  return ::boost::unit_test::unit_test_main(test_suite, argc, argv);
 }
