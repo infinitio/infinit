1e145264c21312e150a99c540a8f4fc37916b123
Fabien <fabien.pichot@infinit.io>
2013-08-30 17:17:14 +0200

    prod: Remove too modern option.

--- prod/prod.sh
+++ prod/prod.sh
@@ -9,11 +9,11 @@ ssd()
 
 start()
 {
-	ssd -1 apertus0.stdout -2 apertus0.stderr -p apertus0.pid -m -b -S ./bin/apertus-server -- --port 0
-	ssd -1 apertus1.stdout -2 apertus1.stderr -p apertus1.pid -m -b -S ./bin/apertus-server -- --port 0
-	ssd -1 apertus2.stdout -2 apertus2.stderr -p apertus2.pid -m -b -S ./bin/apertus-server -- --port 0
-	ssd -1 apertus-proxy.stdout -2 apertus-proxy.stderr -p apertus-proxy.pid -m -b -S ./bin/apertus-server -- --proxy --port 9899
-	ssd -1 tropho.stdout -2 tropho.stderr -p tropho.pid -m -b -S ./bin/trophonius-server -- --port 23456 --control-port 23457
+	ssd -p apertus0.pid -m -b -S ./bin/apertus-server -- --port 0
+	ssd -p apertus1.pid -m -b -S ./bin/apertus-server -- --port 0
+	ssd -p apertus2.pid -m -b -S ./bin/apertus-server -- --port 0
+	ssd -p apertus-proxy.pid -m -b -S ./bin/apertus-server -- --proxy --port 9899
+	ssd -p tropho.pid -m -b -S ./bin/trophonius-server -- --port 23456 --control-port 23457
 	spawn-fcgi -P meta.pid -d ./ -a 127.0.0.1 -p 9002 -- ./bin/meta-server --fcgi --trophonius-control-port 23457 --apertus-host 88.190.48.55 --apertus-port 9899
 	sudo nginx -p ./www -c ../nginx.conf
 }
