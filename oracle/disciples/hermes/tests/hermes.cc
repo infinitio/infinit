44c4033cfd3b69161899b45d077ff14fe6bf1584
Louis Feuvrier <louis.feuvrier@infinit.io>
2013-09-19 15:54:39 +0200

    oracle.hermes: Add and modify tests for store function.

--- oracle/disciples/hermes/tests/hermes.cc
+++ oracle/disciples/hermes/tests/hermes.cc
@@ -37,88 +37,115 @@ test_content(std::string msg, std::string tid, uint64_t id, uint64_t off)
   in.read(output, msg.size());
   in.close();
 
-  std::cout << msg << " =? " << std::string(output) << std::endl;
+  std::cout << msg << ":" << std::string(output) << std::endl;
 
   bool result(std::string(output) == msg);
   delete output;
   return result;
 }
 
-
-BOOST_AUTO_TEST_CASE(test_identification)
+BOOST_AUTO_TEST_CASE(append)
 {
   reactor::Scheduler sched;
 
-  oracle::hermes::TID tid1("transaction1");
-  oracle::hermes::TID tid2("transaction2");
+  oracle::hermes::TID tid("transaction1");
+
+  auto client = [=] (reactor::Thread* serv)
+  {
+    auto& sched = *reactor::Scheduler::scheduler();
+
+    {
+      reactor::network::TCPSocket socket(sched, host, port);
+      infinit::protocol::Serializer s(sched, socket);
+      infinit::protocol::ChanneledStream channels(sched, s);
+
+      oracle::hermes::HermesRPC handler(channels);
+      handler.ident(tid);
+
+      // First simple message.
+      std::string msg1("This is basic content");
+      elle::Buffer input1(msg1.c_str(), msg1.size());
+      BOOST_CHECK_EQUAL(handler.store(1, 0, input1), input1.size());
+      BOOST_CHECK(test_content(msg1, tid, 1, 0));
+
+      // Second message that oerlaps with first one.
+      std::string msg2("content that overlaps");
+      elle::Buffer input2(msg2.c_str(), msg2.size());
+      BOOST_CHECK_EQUAL(handler.store(1, 14, input2), input2.size());
+      BOOST_CHECK(test_content(msg1 + std::string(" that overlaps"), tid, 1, 0));
+
+      // Second message that oerlaps with first one.
+      std::string msg3("overlaps other stuff");
+      elle::Buffer input3(msg3.c_str(), msg3.size());
+      BOOST_CHECK_EQUAL(handler.store(1, 27, input3), input3.size());
+      BOOST_CHECK(test_content(msg1 + std::string(" that overlaps other stuff"), tid, 1, 0));
+    }
+
+    serv->terminate_now();
+  };
+
+  reactor::Thread serv(sched, "hermes", server);
+  reactor::Thread cli(sched, "client", std::bind(client, &serv));
+
+  sched.run();
+}
+
+BOOST_AUTO_TEST_CASE(merge)
+{
+  reactor::Scheduler sched;
+
+  oracle::hermes::TID tid("transaction2");
+
+  auto client = [=] (reactor::Thread* serv)
+  {
+    auto& sched = *reactor::Scheduler::scheduler();
+
+    {
+      reactor::network::TCPSocket socket(sched, host, port);
+      infinit::protocol::Serializer s(sched, socket);
+      infinit::protocol::ChanneledStream channels(sched, s);
+
+      oracle::hermes::HermesRPC handler(channels);
+      handler.ident(tid);
+
+      // First simple message.
+      std::string msg1("This is basic content");
+      elle::Buffer input1(msg1.c_str(), msg1.size());
+      BOOST_CHECK_EQUAL(handler.store(1, 0, input1), input1.size());
+      BOOST_CHECK(test_content(msg1, tid, 1, 0));
+
+      // Second message that oerlaps with first one.
+      std::string msg2("content that overlaps");
+      elle::Buffer input2(msg2.c_str(), msg2.size());
+      BOOST_CHECK_EQUAL(handler.store(1, 14, input2), input2.size());
+      BOOST_CHECK(test_content(msg1 + std::string(" that overlaps"), tid, 1, 0));
+
+      // Second message that oerlaps with first one.
+      std::string msg3("overlaps other stuff");
+      elle::Buffer input3(msg3.c_str(), msg3.size());
+      BOOST_CHECK_EQUAL(handler.store(1, 27, input3), input3.size());
+      BOOST_CHECK(test_content(msg1 + std::string(" that overlaps other stuff"), tid, 1, 0));
+    }
+
+    serv->terminate_now();
+  };
+
+  reactor::Thread serv(sched, "hermes", server);
+  reactor::Thread cli(sched, "client", std::bind(client, &serv));
+
+  sched.run();
+}
+
+BOOST_AUTO_TEST_CASE(prepend)
+{
+  reactor::Scheduler sched;
+
   oracle::hermes::TID tid3("transaction3");
 
   auto client = [=] (reactor::Thread* serv)
   {
     auto& sched = *reactor::Scheduler::scheduler();
 
-    // Test append function.
-    {
-      reactor::network::TCPSocket socket(sched, host, port);
-      infinit::protocol::Serializer s(sched, socket);
-      infinit::protocol::ChanneledStream channels(sched, s);
-
-      oracle::hermes::HermesRPC handler(channels);
-      handler.ident(tid1);
-
-      // First simple message.
-      std::string msg1("This is basic content");
-      elle::Buffer input1(msg1.c_str(), msg1.size());
-      BOOST_CHECK_EQUAL(handler.store(1, 0, input1), input1.size());
-      BOOST_CHECK(test_content(msg1, tid1, 1, 0));
-
-      // Append another message.
-      std::string msg2(" and this is the rest of the content");
-      elle::Buffer input2(msg2.c_str(), msg2.size());
-      BOOST_CHECK_EQUAL(handler.store(1, msg1.size(), input2), input2.size());
-      BOOST_CHECK(test_content(msg1 + msg2, tid1, 1, 0));
-
-      // Append another message.
-      std::string msg3(" followed by even more content.");
-      elle::Buffer input3(msg3.c_str(), msg3.size());
-      BOOST_CHECK_EQUAL(handler.store(1, msg1.size() + msg2.size(), input3), input3.size());
-      BOOST_CHECK(test_content(msg1 + msg2 + msg3, tid1, 1, 0));
-
-      // Append another message.
-      std::string msg4(" MORE!");
-      elle::Buffer input4(msg4.c_str(), msg4.size());
-      BOOST_CHECK_EQUAL(handler.store(1, msg1.size() + msg2.size() + msg3.size(), input4), input4.size());
-      BOOST_CHECK(test_content(msg1 + msg2 + msg3 + msg4, tid1, 1, 0));
-    }
-
-    {
-      reactor::network::TCPSocket socket(sched, host, port);
-      infinit::protocol::Serializer s(sched, socket);
-      infinit::protocol::ChanneledStream channels(sched, s);
-
-      oracle::hermes::HermesRPC handler(channels);
-      handler.ident(tid2);
-
-      // First simple message.
-      std::string msg1("This is basic content");
-      elle::Buffer input1(msg1.c_str(), msg1.size());
-      BOOST_CHECK_EQUAL(handler.store(1, 0, input1), input1.size());
-      BOOST_CHECK(test_content(msg1, tid2, 1, 0));
-
-      // Second message that oerlaps with first one.
-      std::string msg2("content that overlaps");
-      elle::Buffer input2(msg2.c_str(), msg2.size());
-      BOOST_CHECK_EQUAL(handler.store(1, 14, input2), input2.size());
-      BOOST_CHECK(test_content(msg1 + std::string(" that overlaps"), tid2, 1, 0));
-
-      // Second message that oerlaps with first one.
-      std::string msg3("overlaps other stuff");
-      elle::Buffer input3(msg3.c_str(), msg3.size());
-      BOOST_CHECK_EQUAL(handler.store(1, 27, input3), input3.size());
-      BOOST_CHECK(test_content(msg1 + std::string(" that overlaps other stuff"), tid2, 1, 0));
-    }
-
-    // Test prepend function.
     {
       reactor::network::TCPSocket socket(sched, host, port);
       infinit::protocol::Serializer s(sched, socket);
@@ -154,6 +181,127 @@ BOOST_AUTO_TEST_CASE(test_identification)
 
   sched.run();
 }
+
+#if 0
+BOOST_AUTO_TEST_CASE(mix)
+{
+  reactor::Scheduler sched;
+
+  oracle::hermes::TID tid("transaction4");
+
+  auto client = [=] (reactor::Thread* serv)
+  {
+    auto& sched = *reactor::Scheduler::scheduler();
+
+    // Mega mix!
+    {
+      reactor::network::TCPSocket socket(sched, host, port);
+      infinit::protocol::Serializer s(sched, socket);
+      infinit::protocol::ChanneledStream channels(sched, s);
+
+      oracle::hermes::HermesRPC handler(channels);
+      handler.ident(tid);
+
+      //  This |is a message |that |is pretty |cool, |isn't it?|
+      //  _____|_____________|_______________________|
+      //    5        1          6          2  |________________|
+      //                                               4
+
+      // First simple message.
+      std::string msg1("is a message ");
+      elle::Buffer input1(msg1.c_str(), msg1.size());
+      BOOST_CHECK_EQUAL(handler.store(0, 5, input1), input1.size());
+      BOOST_CHECK(test_content(msg1, tid, 0, 5));
+
+      // Push another piece of the message
+      std::string msg2("is pretty cool, ");
+      elle::Buffer input2(msg2.c_str(), msg2.size());
+      BOOST_CHECK_EQUAL(handler.store(0, 23, input2), input2.size());
+      BOOST_CHECK(test_content(msg2, tid, 0, 23));
+
+      // Push an independent message
+      std::string msg3("I am an independ message");
+      elle::Buffer input3(msg3.c_str(), msg3.size());
+      BOOST_CHECK_EQUAL(handler.store(1, 0, input3), input3.size());
+      BOOST_CHECK(test_content(msg3, tid, 1, 0));
+
+      std::string msg4("cool, isn't it?");
+      elle::Buffer input4(msg4.c_str(), msg4.size());
+      BOOST_CHECK_EQUAL(handler.store(0, 33, input4), input4.size());
+      BOOST_CHECK(test_content(msg2 + msg4, tid, 0, 23));
+
+      // Prepend the beginning of the message
+      std::string msg5("This ");
+      elle::Buffer input5(msg5.c_str(), msg5.size());
+      BOOST_CHECK_EQUAL(handler.store(0, 0, input5), input5.size());
+      BOOST_CHECK(test_content(msg5, tid, 0, 0));
+
+      std::string msg6("that ");
+      elle::Buffer input6(msg6.c_str(), msg6.size());
+      BOOST_CHECK_EQUAL(handler.store(0, 18, input6), input6.size());
+      BOOST_CHECK(test_content(msg5 + msg1 + msg6 + msg2 + msg4, tid, 0, 0));
+    }
+
+    serv->terminate_now();
+  };
+
+  reactor::Thread serv(sched, "hermes", server);
+  reactor::Thread cli(sched, "client", std::bind(client, &serv));
+
+  sched.run();
+}
+#endif
+
+BOOST_AUTO_TEST_CASE(simple_fetch)
+{
+  reactor::Scheduler sched;
+
+  oracle::hermes::TID tid("transaction5");
+  std::string msg1("This is basic content");
+
+  auto client = [=] (reactor::Thread* serv)
+  {
+    auto& sched = *reactor::Scheduler::scheduler();
+
+    {
+      reactor::network::TCPSocket socket(sched, host, port);
+      infinit::protocol::Serializer s(sched, socket);
+      infinit::protocol::ChanneledStream channels(sched, s);
+
+      oracle::hermes::HermesRPC handler(channels);
+      handler.ident(tid);
+
+      // First simple message.
+      elle::Buffer input1(msg1.c_str(), msg1.size());
+      BOOST_CHECK_EQUAL(handler.store(1, 0, input1), input1.size());
+      BOOST_CHECK(test_content(msg1, tid, 1, 0));
+
+      // Retrieval of other message directly after.
+      elle::Buffer output1(handler.fetch(1, 0, msg1.size()));
+    }
+
+    // Retrieval of message after reconnexion from another place.
+    {
+      reactor::network::TCPSocket socket(sched, host, port);
+      infinit::protocol::Serializer s(sched, socket);
+      infinit::protocol::ChanneledStream channels(sched, s);
+
+      oracle::hermes::HermesRPC handler(channels);
+      handler.ident(tid);
+
+      // Retrieval of other message directly after.
+      elle::Buffer output1(handler.fetch(1, 0, msg1.size()));
+    }
+
+    serv->terminate_now();
+  };
+
+  reactor::Thread serv(sched, "hermes", server);
+  reactor::Thread cli(sched, "client", std::bind(client, &serv));
+
+  sched.run();
+}
+
 #if 0
 BOOST_AUTO_TEST_CASE(test_identification)
 {
