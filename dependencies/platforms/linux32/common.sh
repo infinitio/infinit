d466677ebab9356b3fac5c0257a5318e53aa1ba7
mefyl <quentin.hocquet@infinit.io>
2013-09-19 18:13:38 +0200

    Update elle.

--- dependencies/platforms/linux32/common.sh
+++
@@ -1,7 +0,0 @@
-# extension
-PLATFORM_LIBRARY_EXTENSION="so"
-set_rpath()
-{
-  echo patchelf --set-rpath $1 $2
-  patchelf --set-rpath $1 $2
-}
--- dependencies/platforms/win64/common.sh
+++
@@ -1,6 +0,0 @@
-# extension
-PLATFORM_LIBRARY_EXTENSION="dll"
-set_rpath()
-{
-    true
-}
