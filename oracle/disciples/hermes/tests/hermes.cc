8f10e2d2e8f480b30aa8c337fede5d384b9d807b
Louis Feuvrier <louis.feuvrier@infinit.io>
2013-09-20 13:29:16 +0200

    oracle.hermes: Add tests handling error cases.

--- oracle/disciples/hermes/tests/hermes.cc
+++ oracle/disciples/hermes/tests/hermes.cc
@@ -9,8 +9,6 @@
 
 #include <oracle/disciples/hermes/src/hermes/Hermes.hh>
 
-// TODO: Try to do a better job at comparing both buffers.
-
 static const std::string base_path = std::string("/tmp/hermes");
 static const char* host = "127.0.0.1";
 static const int port = 4242;
@@ -182,7 +180,6 @@ BOOST_AUTO_TEST_CASE(prepend)
   sched.run();
 }
 
-#if 0
 BOOST_AUTO_TEST_CASE(mix)
 {
   reactor::Scheduler sched;
@@ -202,13 +199,13 @@ BOOST_AUTO_TEST_CASE(mix)
       oracle::hermes::HermesRPC handler(channels);
       handler.ident(tid);
 
-      //  This |is a message |that |is pretty |cool, |isn't it?|
-      //  _____|_____________|_______________________|
-      //    5        1          6          2  |________________|
-      //                                               4
+      // This |is a message that |is pretty |cool, |isn't it?|
+      // _____|__________________|_________________|
+      //   5        1                   2   |________________|
+      //                                              4
 
       // First simple message.
-      std::string msg1("is a message ");
+      std::string msg1("is a message that ");
       elle::Buffer input1(msg1.c_str(), msg1.size());
       BOOST_CHECK_EQUAL(handler.store(0, 5, input1), input1.size());
       BOOST_CHECK(test_content(msg1, tid, 0, 5));
@@ -217,7 +214,7 @@ BOOST_AUTO_TEST_CASE(mix)
       std::string msg2("is pretty cool, ");
       elle::Buffer input2(msg2.c_str(), msg2.size());
       BOOST_CHECK_EQUAL(handler.store(0, 23, input2), input2.size());
-      BOOST_CHECK(test_content(msg2, tid, 0, 23));
+      BOOST_CHECK(test_content(msg1 + msg2, tid, 0, 5));
 
       // Push an independent message
       std::string msg3("I am an independ message");
@@ -228,18 +225,13 @@ BOOST_AUTO_TEST_CASE(mix)
       std::string msg4("cool, isn't it?");
       elle::Buffer input4(msg4.c_str(), msg4.size());
       BOOST_CHECK_EQUAL(handler.store(0, 33, input4), input4.size());
-      BOOST_CHECK(test_content(msg2 + msg4, tid, 0, 23));
+      BOOST_CHECK(test_content(msg1 + "is pretty " + msg4, tid, 0, 5));
 
       // Prepend the beginning of the message
       std::string msg5("This ");
       elle::Buffer input5(msg5.c_str(), msg5.size());
       BOOST_CHECK_EQUAL(handler.store(0, 0, input5), input5.size());
-      BOOST_CHECK(test_content(msg5, tid, 0, 0));
-
-      std::string msg6("that ");
-      elle::Buffer input6(msg6.c_str(), msg6.size());
-      BOOST_CHECK_EQUAL(handler.store(0, 18, input6), input6.size());
-      BOOST_CHECK(test_content(msg5 + msg1 + msg6 + msg2 + msg4, tid, 0, 0));
+      BOOST_CHECK(test_content(msg5 + msg1 + "is pretty " + msg4, tid, 0, 0));
     }
 
     serv->terminate_now();
@@ -250,7 +242,6 @@ BOOST_AUTO_TEST_CASE(mix)
 
   sched.run();
 }
-#endif
 
 BOOST_AUTO_TEST_CASE(simple_fetch)
 {
@@ -302,406 +293,136 @@ BOOST_AUTO_TEST_CASE(simple_fetch)
   sched.run();
 }
 
-#if 0
-BOOST_AUTO_TEST_CASE(test_identification)
-{
-  reactor::Scheduler sched;
-
-  oracle::hermes::TID tid1("transaction1");
-  oracle::hermes::TID tid2("transaction2");
-
-  auto client = [=] (reactor::Thread* serv)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-
-    // Check simple case with identification first.
-    {
-      reactor::network::TCPSocket socket(sched, host, port);
-      infinit::protocol::Serializer s(sched, socket);
-      infinit::protocol::ChanneledStream channels(sched, s);
-
-      oracle::hermes::HermesRPC handler(channels);
-      handler.ident(tid1);
-
-      std::string msg("This is basic content");
-      elle::Buffer input(msg.c_str(), msg.size());
-      BOOST_CHECK_EQUAL(handler.store(1, 1, input), input.size());
-      BOOST_CHECK_EQUAL(handler.fetch(1, 1).size(), input.size());
-    }
-
-    // Try to retrieve previously stored info with different ident number.
-    {
-      reactor::network::TCPSocket socket(sched, host, port);
-      infinit::protocol::Serializer s(sched, socket);
-      infinit::protocol::ChanneledStream channels(sched, s);
-
-      oracle::hermes::HermesRPC handler(channels);
-      handler.ident(tid2);
-
-      // Should throw because tid2 does not have a 1, 1 chunk.
-      BOOST_CHECK_THROW(handler.fetch(1, 1), elle::Exception);
-
-      // When storing a 1, 1 chunk in tid2 we can retrieve it fine.
-      std::string msg("This is some other basic content");
-      elle::Buffer input(msg.c_str(), msg.size());
-      BOOST_CHECK_EQUAL(handler.store(1, 1, input), input.size());
-      BOOST_CHECK_EQUAL(handler.fetch(1, 1).size(), input.size());
-    }
-
-    // Check simple case without identifying first.
-    {
-      reactor::network::TCPSocket socket(sched, host, port);
-      infinit::protocol::Serializer s(sched, socket);
-      infinit::protocol::ChanneledStream channels(sched, s);
-
-      oracle::hermes::HermesRPC handler(channels);
-
-      // Try to store and fetch before identifying.
-      std::string msg("This is yet another basic content");
-      elle::Buffer input(msg.c_str(), msg.size());
-      BOOST_CHECK_THROW(handler.store(1, 2, input), elle::Exception);
-      BOOST_CHECK_THROW(handler.fetch(1, 2), elle::Exception);
-    }
-
-    serv->terminate_now();
-  };
-
-  reactor::Thread serv(sched, "hermes", server);
-  reactor::Thread cli(sched, "client", std::bind(client, &serv));
-
-  sched.run();
-}
-
-BOOST_AUTO_TEST_CASE(test_multiple_chunks)
-{
-  reactor::Scheduler sched;
-
-  oracle::hermes::TID tid("transaction3");
-
-  auto client = [=] (reactor::Thread* serv)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    reactor::network::TCPSocket socket(sched, host, port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
-
-    oracle::hermes::HermesRPC handler(channels);
-
-    // Identify first
-    BOOST_CHECK_NO_THROW(handler.ident(tid));
-
-    // Store first.
-    std::string msg1("This is basic content for first msg");
-    elle::Buffer input1(msg1.c_str(), msg1.size());
-    BOOST_CHECK_EQUAL(handler.store(1, 1, input1), input1.size());
-
-    // Store second.
-    std::string msg2("Little message");
-    elle::Buffer input2(msg2.c_str(), msg2.size());
-    BOOST_CHECK_EQUAL(handler.store(1, 2, input2), input2.size());
-
-    // Fetch second.
-    BOOST_CHECK_EQUAL(handler.fetch(1, 2).size(), input2.size());
-
-    // Store third.
-    std::string msg3("This message is designed to be longer than others.");
-    elle::Buffer input3(msg3.c_str(), msg3.size());
-    BOOST_CHECK_EQUAL(handler.store(1, 3, input3), input3.size());
-
-    // Fetch first.
-    BOOST_CHECK_EQUAL(handler.fetch(1, 1).size(), input1.size());
-
-    // Fetch second.
-    BOOST_CHECK_EQUAL(handler.fetch(1, 2).size(), input2.size());
-
-    // Fetch third.
-    BOOST_CHECK_EQUAL(handler.fetch(1, 3).size(), input3.size());
-
-    serv->terminate_now();
-  };
-
-  reactor::Thread serv(sched, "hermes", server);
-  reactor::Thread cli(sched, "client", std::bind(client, &serv));
-
-  sched.run();
-}
-
-BOOST_AUTO_TEST_CASE(test_filenotfound)
-{
-  reactor::Scheduler sched;
-
-  oracle::hermes::TID tid("transaction4");
-
-  auto client = [=] (reactor::Thread* serv)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    reactor::network::TCPSocket socket(sched, "127.0.0.1", port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
-
-    oracle::hermes::HermesRPC handler(channels);
-    handler.ident(tid);
-
-    BOOST_CHECK_THROW(handler.fetch(1, 1), elle::Exception);
-
-    // Store first file.
-    std::string msg1("This is a common file content. ab cd-ef_gh");
-    elle::Buffer input1(msg1.c_str(), msg1.size());
-    BOOST_CHECK_EQUAL(handler.store(1, 1, input1), input1.size());
-
-    // Try to fetch inexisting file.
-    BOOST_CHECK_THROW(handler.fetch(1, 2), elle::Exception);
-
-    // Store second file.
-    std::string msg2("This is another common file chunk or whatever");
-    elle::Buffer input2(msg2.c_str(), msg2.size());
-    BOOST_CHECK_EQUAL(handler.store(1, 2, input2), input2.size());
-
-    // Fetch existing files.
-    BOOST_CHECK_NO_THROW(handler.fetch(1, 1));
-    BOOST_CHECK_NO_THROW(handler.fetch(1, 2));
-    BOOST_CHECK_EQUAL(handler.fetch(1, 1).size(), input1.size());
-    BOOST_CHECK_EQUAL(handler.fetch(1, 2).size(), input2.size());
-
-    serv->terminate_now();
-  };
-
-  reactor::Thread serv(sched, "hermes", server);
-  reactor::Thread cli(sched, "client", std::bind(client, &serv));
-
-  sched.run();
-}
-
-BOOST_AUTO_TEST_CASE(test_file_already_present)
-{
-  reactor::Scheduler sched;
-
-  oracle::hermes::TID tid("transaction5");
-
-  auto client = [=] (reactor::Thread* serv)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    reactor::network::TCPSocket socket(sched, "127.0.0.1", port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
-
-    oracle::hermes::HermesRPC handler(channels);
-    handler.ident(tid);
-
-    // Store and fetch first file.
-    std::string msg1("This is a random file as well");
-    elle::Buffer input1(msg1.c_str(), msg1.size());
-    BOOST_CHECK_EQUAL(handler.store(4, 1, input1), input1.size());
-    BOOST_CHECK_EQUAL(handler.fetch(4, 1).size(), input1.size());
-
-    // Try and store a file with the same FileID/Offset pair.
-    std::string msg2("This file cannot be created");
-    elle::Buffer input2(msg2.c_str(), msg2.size());
-    BOOST_CHECK_THROW(handler.store(4, 1, input2), elle::Exception);
-
-    // Fetch first file and check it's actually the first file.
-    BOOST_CHECK_NO_THROW(handler.fetch(4, 1));
-    BOOST_CHECK_EQUAL(handler.fetch(4, 1).size(), input1.size());
-
-    serv->terminate_now();
-  };
-
-  reactor::Thread serv(sched, "hermes", server);
-  reactor::Thread cli(sched, "client", std::bind(client, &serv));
-
-  sched.run();
-}
-
-BOOST_AUTO_TEST_CASE(test_bad_basepath)
-{
-  reactor::Scheduler sched;
-
-  auto tmp_server = [=] ()
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    BOOST_CHECK_THROW(oracle::hermes::Hermes herm(sched, port, "/dev/null"),
-                      elle::Exception);
-  };
-
-  reactor::Thread serv(sched, "hermes", tmp_server);
-
-  sched.run();
-}
-
-BOOST_AUTO_TEST_CASE(test_restart)
+BOOST_AUTO_TEST_CASE(partial_fetch)
 {
   reactor::Scheduler sched;
 
   oracle::hermes::TID tid("transaction6");
+  std::string msg1("content");
 
-  auto tmp_server = [=] (reactor::Thread* to_wait)
+  auto client = [=] (reactor::Thread* serv)
   {
     auto& sched = *reactor::Scheduler::scheduler();
-    sched.current()->wait(*to_wait);
 
-    oracle::hermes::Hermes(sched, port, base_path).run();
-  };
+    {
+      reactor::network::TCPSocket socket(sched, host, port);
+      infinit::protocol::Serializer s(sched, socket);
+      infinit::protocol::ChanneledStream channels(sched, s);
 
-  auto first_client = [=] (reactor::Thread* serv)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    reactor::network::TCPSocket socket(sched, host, port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
+      oracle::hermes::HermesRPC handler(channels);
+      handler.ident(tid);
 
-    oracle::hermes::HermesRPC handler(channels);
-    handler.ident(tid);
+      // First simple message.
+      elle::Buffer input1(msg1.c_str(), msg1.size());
+      BOOST_CHECK_EQUAL(handler.store(0, 0, input1), input1.size());
+      BOOST_CHECK(test_content(msg1, tid, 0, 0));
 
-    std::string msg("Hello there, I am a bit of file.");
-    elle::Buffer input(msg.c_str(), msg.size());
-    BOOST_CHECK_EQUAL(handler.store(5, 1, input), input.size());
-    BOOST_CHECK_EQUAL(handler.fetch(5, 1).size(), input.size());
+      // Retrieval of message directly after.
+      elle::Buffer output1;
+      BOOST_CHECK_NO_THROW(output1 = handler.fetch(0, 0, msg1.size() + 2));
+      BOOST_CHECK_EQUAL(output1.size(), msg1.size());
+    }
 
     serv->terminate_now();
   };
 
-  auto second_client = [=] (reactor::Thread* to_wait, reactor::Thread* serv)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    sched.current()->wait(*to_wait);
-
-    reactor::network::TCPSocket socket(sched, host, port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
-
-    oracle::hermes::HermesRPC handler(channels);
-    handler.ident(tid);
-
-    BOOST_CHECK_NO_THROW(handler.fetch(5, 1));
-    // 32 is the size of the previously stored file.
-    BOOST_CHECK_EQUAL(handler.fetch(5, 1).size(), 32);
-
-    serv->terminate_now();
-  };
-
-  reactor::Thread serv1(sched, "hermes1", server);
-  reactor::Thread cli1(sched, "client1", std::bind(first_client, &serv1));
-
-  reactor::Thread serv2(sched, "hermes2", std::bind(tmp_server, &cli1));
-
-  reactor::Thread cli2(sched, "client2", std::bind(second_client,
-                                                   &cli1,
-                                                   &serv2));
+  reactor::Thread serv(sched, "hermes", server);
+  reactor::Thread cli(sched, "client", std::bind(client, &serv));
 
   sched.run();
 }
 
-BOOST_AUTO_TEST_CASE(test_restart_fail)
+BOOST_AUTO_TEST_CASE(fail_fetch)
 {
   reactor::Scheduler sched;
-  int port(4242);
 
-  auto first_server = [=] ()
+  oracle::hermes::TID tid("transaction7");
+  std::string msg1("content");
+
+  auto client = [=] (reactor::Thread* serv)
   {
     auto& sched = *reactor::Scheduler::scheduler();
-    oracle::hermes::Hermes herm(sched, port, base_path);
 
-    herm.run();
-  };
+    {
+      reactor::network::TCPSocket socket(sched, host, port);
+      infinit::protocol::Serializer s(sched, socket);
+      infinit::protocol::ChanneledStream channels(sched, s);
 
-  auto second_server = [=] (reactor::Thread* to_wait)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    sched.current()->wait(*to_wait);
+      oracle::hermes::HermesRPC handler(channels);
+      handler.ident(tid);
 
-    oracle::hermes::Hermes herm(sched, port, base_path);
+      // First simple message.
+      elle::Buffer input1(msg1.c_str(), msg1.size());
+      BOOST_CHECK_EQUAL(handler.store(0, 0, input1), input1.size());
+      BOOST_CHECK(test_content(msg1, tid, 0, 0));
 
-    herm.run();
-  };
-
-  auto first_client = [=] (reactor::Thread* serv)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    reactor::network::TCPSocket socket(sched, "127.0.0.1", port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
-
-    oracle::hermes::HermesRPC handler(channels);
-
-    std::string msg("Hello there, I am a bit of file.");
-    elle::Buffer input(msg.c_str(), msg.size());
-    BOOST_CHECK_EQUAL(handler.store(6, 1, input), input.size());
-    BOOST_CHECK_EQUAL(handler.fetch(6, 1).size(), input.size());
+      // Try to retrieve unexisting message.
+      elle::Buffer output1;
+      BOOST_CHECK_THROW(output1 = handler.fetch(1, 0, msg1.size()),
+                        elle::Exception);
+    }
 
     serv->terminate_now();
   };
 
-  auto second_client = [=] (reactor::Thread* to_wait, reactor::Thread* serv)
-  {
-    auto& sched = *reactor::Scheduler::scheduler();
-    sched.current()->wait(*to_wait);
-
-    reactor::network::TCPSocket socket(sched, "127.0.0.1", port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
-
-    oracle::hermes::HermesRPC handler(channels);
-
-    BOOST_CHECK_THROW(handler.fetch(99, 1), elle::Exception);
-
-    serv->terminate_now();
-  };
-
-  reactor::Thread serv1(sched, "hermes1", first_server);
-  reactor::Thread cli1(sched, "client1", std::bind(first_client, &serv1));
-
-  reactor::Thread serv2(sched, "hermes2", std::bind(second_server, &cli1));
-  reactor::Thread cli2(sched, "client2", std::bind(second_client, &cli1, &serv2));
+  reactor::Thread serv(sched, "hermes", server);
+  reactor::Thread cli(sched, "client", std::bind(client, &serv));
 
   sched.run();
-  //clean(base_path);
 }
 
-BOOST_AUTO_TEST_CASE(test_parallel)
+BOOST_AUTO_TEST_CASE(fail_store)
 {
   reactor::Scheduler sched;
-  int port(4242);
 
-  auto server = [&] ()
+  oracle::hermes::TID tid("transaction8");
+  std::string msg1("content");
+
+  auto client = [=] (reactor::Thread* serv)
   {
-    oracle::hermes::Hermes(sched, port, base_path).run();
-  };
+    auto& sched = *reactor::Scheduler::scheduler();
 
-  auto client1 = [&] (reactor::Thread* serv)
-  {
-    reactor::network::TCPSocket socket(sched, "127.0.0.1", port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
+    {
+      reactor::network::TCPSocket socket(sched, host, port);
+      infinit::protocol::Serializer s(sched, socket);
+      infinit::protocol::ChanneledStream channels(sched, s);
 
-    oracle::hermes::HermesRPC handler(channels);
+      oracle::hermes::HermesRPC handler(channels);
+      handler.ident(tid);
 
-    std::string msg("Hello there, I am a bit of another file.");
-    elle::Buffer input(msg.c_str(), msg.size());
-    BOOST_CHECK_EQUAL(handler.store(7, 1, input), input.size());
+      // First simple message.
+      elle::Buffer input1(msg1.c_str(), msg1.size());
+      BOOST_CHECK_EQUAL(handler.store(0, 0, input1), input1.size());
+      BOOST_CHECK(test_content(msg1, tid, 0, 0));
 
-    sched.current()->wait(*serv);
-  };
-
-  auto client2 = [&] (reactor::Thread* serv)
-  {
-    reactor::network::TCPSocket socket(sched, "127.0.0.1", port);
-    infinit::protocol::Serializer s(sched, socket);
-    infinit::protocol::ChanneledStream channels(sched, s);
-
-    oracle::hermes::HermesRPC handler(channels);
-
-    std::string msg("Hello there, I am a bit of yet another file.");
-    elle::Buffer input(msg.c_str(), msg.size());
-    BOOST_CHECK_EQUAL(handler.store(7, 2, input), input.size());
+      // Try to store the message once again with the same id.
+      elle::Buffer input2(msg1.c_str(), msg1.size());
+      BOOST_CHECK_THROW(handler.store(0, 0, input1), elle::Exception);
+      BOOST_CHECK(test_content(msg1, tid, 0, 0));
+    }
 
     serv->terminate_now();
   };
 
-  reactor::Thread serv(sched, "serv", server);
-  reactor::Thread cli1(sched, "client1", std::bind(client1, &serv));
-  reactor::Thread cli2(sched, "client2", std::bind(client2, &serv));
+  reactor::Thread serv(sched, "hermes", server);
+  reactor::Thread cli(sched, "client", std::bind(client, &serv));
 
   sched.run();
-  //clean(base_path);
 }
-#endif
+
+BOOST_AUTO_TEST_CASE(fail_boot)
+{
+  reactor::Scheduler sched;
+
+  oracle::hermes::TID tid("transaction9");
+
+  auto serv2 = [=]
+  {
+    auto& sched = *reactor::Scheduler::scheduler();
+    BOOST_CHECK_THROW(oracle::hermes::Hermes(sched, port, "/dev/null").run(),
+                      elle::Exception);
+  };
+
+  reactor::Thread serv(sched, "hermes", serv2);
+  sched.run();
+}
