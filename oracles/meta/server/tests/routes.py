fc072e5096e3f3a1b4e7b26e0f1981b4704210e0
Antony MECHIN <antony.mechin@infinit.io>
2013-10-09 18:25:23 +0200

    oracles.meta.server: Add routes test.

--- oracles/meta/server/tests/routes.py
+++ oracles/meta/server/tests/routes.py
@@ -10,7 +10,6 @@ def throws(f, status):
   except Meta.Exception as e:
     if status != 200:
       assert isinstance(e.status, int)
-      print(status, e.status)
       assert e.status == status
 
 with Meta() as meta:
@@ -19,21 +18,21 @@ with Meta() as meta:
   throws(lambda: meta.get('user/logout'), 200)
   throws(lambda: meta.post('user/register', {}), 400)
   throws(lambda: meta.post('user/search', {}), 400)
-  throws(lambda: meta.get('user/<id_or_email>/view'), 200)
+  throws(lambda: meta.get('user/email@email.com/view'), 200)
   throws(lambda: meta.post('user/from_public_key', {}), 400)
-  throws(lambda: meta.get('user/swaggers'), 200)
-  throws(lambda: meta.post('user/remove_swagger', {}), 400)
-  throws(lambda: meta.post('user/favorite', {}), 400)
-  throws(lambda: meta.post('user/unfavorite', {}), 400)
-  throws(lambda: meta.post('user/edit', {}), 400)
+  throws(lambda: meta.get('user/swaggers'), 403)
+  throws(lambda: meta.post('user/remove_swagger', {}), 403)
+  throws(lambda: meta.post('user/favorite', {}), 403)
+  throws(lambda: meta.post('user/unfavorite', {}), 403)
+  throws(lambda: meta.post('user/edit', {}), 403)
   throws(lambda: meta.post('user/invite', {}), 400)
   throws(lambda: meta.get('user/invited'), 200)
   throws(lambda: meta.get('self'), 200)
   throws(lambda: meta.get('minimumself'), 200)
   throws(lambda: meta.get('user/remaining_invitations'), 200)
-  throws(lambda: meta.get('user/id/avatar'), 200)
-  throws(lambda: meta.post('user/id/avatar', {}), 200)
-  throws(lambda: meta.post('debug', {}), 400)
+  # throws(lambda: meta.get('user/5241b9d226b9f10fff8be391/avatar'), 200)
+  # throws(lambda: meta.post('user/5241b9d226b9f10fff8be391/avatar', {}), 200)
+  throws(lambda: meta.post('debug', {}), 403)
   throws(lambda: meta.post('user/add_swagger', {}), 400)
   throws(lambda: meta.post('user/connect', {}), 400)
   throws(lambda: meta.post('user/disconnect', {}), 400)
