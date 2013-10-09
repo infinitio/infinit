7daacb2aec4cf19a8e0472de7c4ca455e293cf1c
Antony MECHIN <antony.mechin@infinit.io>
2013-10-09 17:14:08 +0200

    oracles.meta.server: Add 'has_optionals' to expect_json.

--- oracles/meta/server/src/infinit/oracles/meta/utils.py
+++ oracles/meta/server/src/infinit/oracles/meta/utils.py
@@ -1,25 +1,32 @@
 import bottle
 
 class expect_json:
-  def __init__(self, expect_keys = [], explode_keys = []):
+
+  def __init__(self, expect_keys = [], explode_keys = [], has_optionals = False):
     assert isinstance(expect_keys, (list, set))
     assert isinstance(explode_keys, (list, set))
     self.mandatory_fields = set(expect_keys + explode_keys)
     self.explode_keys = explode_keys
+    self.has_optionals = has_optionals
+
   def __call__(self, method, *args, **kwargs):
-    def decorator_wrapper(*args, **kwargs):
+    def decorator_wrapper(_self, *args, **kwargs):
       try:
         input = bottle.request.json
         if input is None:
-          bad_request("body is not JSON")
+          bottle.abort(400, "body is not JSON")
       except ValueError:
-        bad_request("body can't be decoded as JSON")
+        bottle.abort(400, "body can't be decoded as JSON")
       json = dict(input)
       for key in self.mandatory_fields:
         if key not in json.keys():
-          bad_request("missing key: %r" % key)
-      # Probably optimisable.
+          bottle.abort(400, "missing key: %r" % key)
       exploded = {key: json[key] for key in self.explode_keys}
-      optionals = {key: json[key] for key in json.keys() - self.explode_keys}
-      return method(*args, optionals = optionals, **exploded)
+      if self.has_optionals:
+        # Probably optimisable.
+        optionals = {key: json[key] for key in json.keys() - self.explode_keys}
+        print(args, optionals, exploded)
+        return method(_self, *args, optionals = optionals, **exploded)
+      else:
+        return method(_self, *args, **exploded)
     return decorator_wrapper
