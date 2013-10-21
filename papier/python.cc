5a0377e942e835f6bb5ca6a23cee60b71b083f6c
Antony MECHIN <antony.mechin@infinit.io>
2013-10-21 16:10:20 +0200

    papier: Add python binding for identity and passport generation.

---
+++ papier/python.cc
@@ -0,0 +1,112 @@
+#include <wrappers/boost/python.hh>
+
+#include <papier/Authority.hh>
+#include <cryptography/PublicKey.hh>
+#include <papier/Identity.hh>
+#include <papier/Passport.hh>
+
+#include <string>
+
+using namespace boost::python;
+typedef boost::python::return_value_policy<boost::python::return_by_value>
+  by_value;
+
+static
+std::string
+passport(std::string const& id,
+         std::string const& name,
+         std::string const& public_key,
+         std::string const& authority_file,
+         std::string const& authority_password)
+{
+  // Load the authority file.
+  papier::Authority authority{elle::io::Path{authority_file}};
+
+  // decrypt the authority.
+  if (authority.Decrypt(authority_password) == elle::Status::Error)
+    throw std::runtime_error("unable to decrypt the authority");
+
+  cryptography::PublicKey pubkey{};
+  pubkey.Restore(public_key);
+
+  papier::Passport passport{id, name, pubkey, authority};
+
+  std::string passport_string;
+  if (passport.Save(passport_string) != elle::Status::Error)
+    return passport_string;
+
+  throw std::runtime_error("unabled to save the passport");
+}
+
+static
+//std::tuple<std::string, std::string>
+boost::python::tuple
+identity(std::string const& id,
+         std::string const& authority_file,
+         std::string const& authority_password,
+         std::string const& login,
+         std::string const& password)
+{
+  cryptography::KeyPair pair =
+    cryptography::KeyPair::generate(cryptography::Cryptosystem::rsa,
+                                    papier::Identity::keypair_length);
+
+  elle::io::Path authority_path;
+  papier::Identity identity;
+
+  // check the argument.
+  if (login.empty() == true)
+    throw std::runtime_error("unable to create a user without a user name");
+
+  if (authority_path.Create(authority_file) == elle::Status::Error)
+    throw std::runtime_error("unable to create authority path");
+
+  // Load the authority file.
+  papier::Authority authority(authority_path);
+
+  // decrypt the authority.
+  if (authority.Decrypt(authority_password) == elle::Status::Error)
+    throw std::runtime_error("unable to decrypt the authority");
+
+  // create the identity.
+  if (identity.Create(id, login, pair) == elle::Status::Error)
+    throw std::runtime_error("unable to create the identity");
+
+  // encrypt the identity.
+  if (identity.Encrypt(password) == elle::Status::Error)
+    throw std::runtime_error("unable to encrypt the identity");
+
+  // seal the identity.
+  if (identity.Seal(authority) == elle::Status::Error)
+    throw std::runtime_error("unable to seal the identity");
+
+  std::string all, pub;
+  if (identity.Save(all) != elle::Status::Error &&
+      identity.pair().K().Save(pub) != elle::Status::Error)
+    return boost::python::make_tuple(all, pub);
+
+  throw std::runtime_error("unable to save public key and identity");
+}
+
+void export_passport();
+void export_passport()
+{
+  def("generate_passport", &passport, by_value());
+}
+
+void export_identity();
+void export_identity()
+{
+  def("generate_identity", &identity, by_value());
+}
+
+extern "C"
+{
+  PyObject* PyInit_papier();
+}
+
+BOOST_PYTHON_MODULE(papier)
+{
+  export_identity();
+  export_passport();
+}
