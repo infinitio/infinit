71b2efde5931d4f1611a387efd40e2eb3743d598
Louis Feuvrier <louis.feuvrier@infinit.io>
2013-10-24 12:27:27 +0200

    oracles.hermes.client: Add incomplete download procedure

--- oracles/hermes/client/src/infinit/oracles/hermes/HermesClient.cc
+++ oracles/hermes/client/src/infinit/oracles/hermes/HermesClient.cc
@@ -40,7 +40,7 @@ namespace oracles
 
         while (file.second.progress() != file.second.file_size())
         {
-          Size tr = 500;
+          Size tr = 512;
 
           // In the case of a ConnexionClosed interruption indicating that the
           // recipient has reconnected, the last block might be stored on the
@@ -54,6 +54,85 @@ namespace oracles
             snap.increment_progress(file.first, tr);
         }
       }
+    }
+
+    void
+    HermesClient::download(boost::filesystem::path const& snaploc, Size off)
+    {
+      std::string strsl = snaploc.string();
+      frete::TransferSnapshot snap(elle::serialize::from_file(strsl));
+
+      // Compare the new offset with the old one.
+      find_holes(snap, off);
+
+      oracles::hermes::HermesRPC handler(*_channels);
+      handler.ident(_tid);
+
+      while (not _holes.empty())
+      {
+        auto hole = _holes.begin();
+        FileID id(hole->first.first);
+        Size off(hole->first.second);
+        Size size(hole->second);
+
+        elle::Buffer inc(handler.fetch(id, off, size));
+
+        std::ofstream out(get_path(snap, id));
+        out.seekp(off);
+
+        out.write(reinterpret_cast<char*>(inc.mutable_contents()),
+                  inc.size());
+        out.close();
+
+        _holes.erase(hole);
+      }
+    }
+
+    void
+    HermesClient::find_holes(frete::TransferSnapshot& snap, Size off)
+    {
+      if (off == snap.progress())
+        return;
+
+      Size tot = 0;
+      for (const auto& trs : snap.transfers())
+      {
+        Size tmp = tot;
+        tot += trs.second.file_size();
+
+        if (not snap.progress() < tot)
+          continue;
+
+        Size loc_off = 0;
+        if (snap.progress() > tmp)
+          loc_off = snap.progress() % tmp;
+
+        if (off <= tot)
+        {
+          _holes[{trs.first, loc_off}] = trs.second.file_size() - (tot - off);
+          break;
+        }
+        else
+          _holes[{trs.first, loc_off}] = trs.second.file_size() - loc_off;
+      }
+
+      // Set the overall progress of the snapshot to the current offset of
+      // the sender.
+      snap.progress(off);
+    }
+
+    std::string
+    HermesClient::get_path(frete::TransferSnapshot& snap, FileID id) const
+    {
+      for (const auto& trs : snap.transfers())
+      {
+        if (trs.first != id)
+          continue;
+
+        return trs.second.path();
+      }
+
+      return std::string("/dev/null");
     }
   }
 }
--- oracles/hermes/client/src/infinit/oracles/hermes/HermesClient.hh
+++ oracles/hermes/client/src/infinit/oracles/hermes/HermesClient.hh
@@ -28,8 +28,22 @@ namespace oracles
       void
       upload(boost::filesystem::path const& snaploc);
 
+      void
+      download(boost::filesystem::path const& snaploc, Size off);
+
+    private:
+      void
+      find_holes(frete::TransferSnapshot& snap, Size off);
+
+      std::string
+      get_path(frete::TransferSnapshot& snap, FileID id) const;
+
     private:
       TID _tid;
+
+      // List of holes.
+    private:
+      std::map<std::pair<FileID, Offset>, Size> _holes;
 
       // Socket, Serializer, Stream and RPC stuff.
     private:
