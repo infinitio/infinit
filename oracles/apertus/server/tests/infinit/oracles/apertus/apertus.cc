645dfdb437b5b0df3498021b5e3cef264bfa8002
Louis Feuvrier <louis.feuvrier@infinit.io>
2013-11-12 13:47:10 +0100

    oracles.apertus: Add basic tests.

--- oracles/apertus/server/tests/infinit/oracles/apertus/apertus.cc
+++ oracles/apertus/server/tests/infinit/oracles/apertus/apertus.cc
@@ -7,7 +7,7 @@
 BOOST_AUTO_TEST_CASE(basic)
 {
   reactor::Scheduler sched;
-  oracles::apertus::Apertus ap(sched, "", 0, "127.0.0.1", 6565);
+  oracles::apertus::Apertus* ap = new oracles::apertus::Apertus(sched, "", 0, "127.0.0.1", 6565);
 
   auto client = [&] (reactor::Thread* serv)
   {
@@ -21,7 +21,13 @@ BOOST_AUTO_TEST_CASE(basic)
     elle::ConstWeakBuffer tidbuffer(tid, tidsize);
     sock.write(tidbuffer);
 
-    
+    sched.current()->yield();
+
+    auto& haha = ap->get_clients();
+    BOOST_CHECK(haha.find(std::string(tid)) != haha.end());
+    BOOST_CHECK(haha[std::string(tid)] != nullptr);
+
+    serv->terminate_now();
   };
 
   reactor::Thread* serv = new reactor::Thread(sched, "serv",
@@ -29,4 +35,76 @@ BOOST_AUTO_TEST_CASE(basic)
   new reactor::Thread(sched, "client", std::bind(client, serv));
 
   sched.run();
+  delete ap;
+}
+
+BOOST_AUTO_TEST_CASE(exchange)
+{
+  reactor::Scheduler sched;
+  oracles::apertus::Apertus* ap = new oracles::apertus::Apertus(sched, "", 0, "127.0.0.1", 6566);
+
+  auto client1 = [&] (reactor::Thread* serv)
+  {
+    reactor::network::TCPSocket sock("127.0.0.1", 6566);
+
+    char tidsize = 6;
+    elle::ConstWeakBuffer sizebuffer(&tidsize, 1);
+    sock.write(sizebuffer);
+
+    const char* tid = "hitler";
+    elle::ConstWeakBuffer tidbuffer(tid, tidsize);
+    sock.write(tidbuffer);
+
+    sched.current()->yield();
+
+    auto& haha = ap->get_clients();
+    BOOST_CHECK(haha.find(std::string(tid)) != haha.end());
+    BOOST_CHECK(haha[std::string(tid)] != nullptr);
+
+    const char* msg1 = "firt_message";
+    elle::ConstWeakBuffer msg_buffer1(msg1, 13);
+    sock.write(msg_buffer1);
+
+    sched.current()->yield();
+    sched.current()->yield();
+    sched.current()->yield();
+    sched.current()->yield();
+    sched.current()->yield();
+    sched.current()->yield();
+  };
+
+  auto client2 = [&] (reactor::Thread* serv)
+  {
+    sched.current()->yield();
+    reactor::network::TCPSocket sock("127.0.0.1", 6566);
+
+    char tidsize = 6;
+    elle::ConstWeakBuffer sizebuffer(&tidsize, 1);
+    sock.write(sizebuffer);
+
+    const char* tid = "hitler";
+    elle::ConstWeakBuffer tidbuffer(tid, tidsize);
+    sock.write(tidbuffer);
+
+    sched.current()->yield();
+
+    auto& haha = ap->get_clients();
+
+    char content[13];
+    reactor::network::Buffer tmp(content, 13);
+    sock.read(tmp);
+
+    BOOST_CHECK_EQUAL(std::string(content), std::string("firt_message"));
+
+    serv->terminate_now();
+  };
+
+  reactor::Thread* serv = new reactor::Thread(sched, "serv",
+    std::bind(&oracles::apertus::Apertus::run, ap));
+
+  new reactor::Thread(sched, "client1", std::bind(client1, serv));
+  new reactor::Thread(sched, "client2", std::bind(client2, serv));
+
+  sched.run();
+  delete ap;
 }
