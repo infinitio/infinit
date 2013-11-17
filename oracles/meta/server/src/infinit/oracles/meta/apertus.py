5dea36c2e6d61e785cb2f283814e505c8008e416
Antony MECHIN <antony.mechin@infinit.io>
2013-11-17 13:53:36 +0100

    oracles.meta.server.Apertus: Add some logs.

--- oracles/meta/server/src/infinit/oracles/meta/apertus.py
+++ oracles/meta/server/src/infinit/oracles/meta/apertus.py
@@ -3,10 +3,13 @@
 import bson
 import uuid
 import random
+import time
 from . import conf, error, regexp
 from .utils import api, require_admin, require_logged_in
+import elle.log
 
 # XXX: Make it generic with trophonius.
+ELLE_LOG_COMPONENT = 'infinit.oracles.meta.server.Apertus'
 
 class Mixin:
 
@@ -16,18 +19,25 @@ class Mixin:
                   port):
     """Register a apertus.
     """
-    print("aggregate apertus %s" % uid)
     assert isinstance(uid, uuid.UUID)
     # Upsert is important here  be cause if a apertus crashed and didn't
     # unregister itself, it's important to update the old entry in the database.
-    res = self.database.apertus.insert(
+    res = self.database.apertus.update(
+      {
+        '_id': str(uid),
+      },
       {
         '_id': str(uid),
         'ip': self.remote_ip,
         'port': port,
+        'time': time.time(),
       },
       upsert = True,
     )
+    if res['updatedExisting']:
+      elle.log.dump("ping from trophonius: %s" % uid)
+    else:
+      elle.log.log("register trophonius %s" % uid)
     return self.success()
 
   @api('/apertus/<uid>', method = 'DELETE')
@@ -35,13 +45,13 @@ class Mixin:
                      uid: uuid.UUID):
     """Unregister a apertus.
     """
-    print("delete apertus %s" % uid)
-    assert isinstance(uid, uuid.UUID)
-    self.database.transactions.update({'fallback': str(uid)},
-                                      {'$set': {'fallback': None}},
-                                      multi = True)
-    res = self.database.apertus.remove({"_id": str(uid)})
-    return self.success()
+    with elle.log.log("unregister trophonius %s" % uid):
+      assert isinstance(uid, uuid.UUID)
+      self.database.transactions.update({'fallback': str(uid)},
+                                        {'$set': {'fallback': None}},
+                                        multi = True)
+      res = self.database.apertus.remove({"_id": str(uid)})
+      return self.success()
 
   def choose_apertus(self, transaction_id):
     pass
@@ -52,29 +62,46 @@ class Mixin:
                            id: bson.ObjectId):
     """Return the selected apertus ip/port for a given transaction_id.
     """
-    user = self.user
+    with elle.log.trace("get fallback for transaction %s" % id):
+      user = self.user
 
-    transaction = self.transaction(id, user['_id'])
+      transaction = self.transaction(id, user['_id'])
 
-    if transaction.get('fallback') is None:
-      apertus = self.database.apertus.find(fields = ['ip', 'port'])
-      if apertus.count() == 0:
-        return self.fail(error.NO_APERTUS)
-      index = random.randint(0, apertus.count() - 1)
-      fallback = apertus[index]
-      fallback = '%s:%s' % (fallback['ip'], fallback['port'])
-
-      _transaction = self.database.transactions.find_and_modify(
-        {
-          '_id': id,
-          'fallback': None,
-        },
-        {
-          '$set': {'fallback': fallback},
-        },
-        new = True,
-      )
-      if _transaction is None: # Race condition.
+      if transaction.get('fallback') is None:
+        elle.log.debug('choosing a random apertus')
+        apertus = self.database.apertus.find(fields = ['ip', 'port'])
+        if apertus.count() == 0:
+          return self.fail(error.NO_APERTUS)
+        index = random.randint(0, apertus.count() - 1)
+        fallback = apertus[index]
+        elle.log.debug('selected fallback: %s' % fallback)
+        fallback = '%s:%s' % (fallback['ip'], fallback['port'])
+        _transaction = self.database.transactions.find_and_modify(
+          {
+            '_id': id,
+            'fallback': None,
+          },
+          {
+            '$set': {'fallback': fallback},
+          },
+          new = True,
+        )
+        if _transaction is None: # Race condition.
+          elle.log.warn('transaction has been modified before')
+          _transaction = self.database.transactions.find_and_modify(
+            {
+              '_id': id,
+              'fallback': fallback,
+            },
+            {
+              '$set': {'fallback': None},
+            },
+            new = False,
+          )
+          fallback = _transaction['fallback']
+      else:
+        fallback = transaction['fallback']
+        elle.log.debug('got fallback from transaction: %s' % fallback)
         _transaction = self.database.transactions.find_and_modify(
           {
             '_id': id,
@@ -83,22 +110,10 @@ class Mixin:
           {
             '$set': {'fallback': None},
           },
-          new = False,
+          new = True,
         )
-        fallback = _transaction['fallback']
-    else:
-      fallback = transaction['fallback']
-      _transaction = self.database.transactions.find_and_modify(
-        {
-          '_id': id,
-          'fallback': fallback,
-        },
-        {
-          '$set': {'fallback': None},
-        },
-        new = True,
-      )
-    return self.success({'fallback': fallback})
+
+      return self.success({'fallback': fallback})
 
   @api('/apertus')
   def registered_apertus(self):
