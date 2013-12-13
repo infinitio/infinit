771f71f0388100b03fe27fed0305b7db96b0cf4c
mefyl <quentin.hocquet@infinit.io>
2013-12-13 13:32:14 +0100

    oracles.meta.client: Fix test.

--- oracles/meta/client/tests/meta_client.cc
+++ oracles/meta/client/tests/meta_client.cc
@@ -1,8 +1,16 @@
+#include <elle/test.hh>
+
+#include <reactor/scheduler.hh>
+
 #include <infinit/oracles/meta/Client.hh>
 
-int main(void)
+ELLE_TEST_SCHEDULED(connection_refused)
 {
   infinit::oracles::meta::Client c("127.0.0.1", 21232);
+}
 
-
+ELLE_TEST_SUITE()
+{
+  auto& suite = boost::unit_test::framework::master_test_suite();
+  suite.add(BOOST_TEST_CASE(connection_refused));
 }
