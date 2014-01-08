290905f009da26447a82b88fe2b9675995130cf3
mefyl <quentin.hocquet@infinit.io>
2014-01-08 15:05:33 +0100

    Remove dead oracle directory.

--- oracle/disciples/pythia/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-"""Simple meta API client"""
-
-
-from pythia.client import Client
-from pythia.admin import Admin
-import pythia.constants
--- oracle/disciples/pythia/constants.py
+++
@@ -1,7 +0,0 @@
-# -*- encoding: utf-8 -*-
-import os
-
-ADMIN_TOKEN = "fdjskfdakl;asdklwqioefwiopfdsjkl;daskl;askl;fsd"
-_META_PORT = 'INFINIT_META_PORT' in os.environ and os.environ['INFINIT_META_PORT'] or 12345
-DEFAULT_SERVER = 'http://localhost:%i' % int(_META_PORT)
-RESET_PASSWORD_VALIDITY = 2 * 3600 # 2 hours
