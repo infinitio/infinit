99d51b06f2b6e67babe226abb70f07157bb084f9
Christopher Crone <christopher.crone@infinit.io>
2014-02-18 17:30:40 +0100

    farm.targets: Test frete and station on the buildfarm.

--- farm/targets.py
+++ farm/targets.py
@@ -4,6 +4,8 @@ arch, osyst, comp = os.environ['BUILDFARM_NAME'].split('-')
 
 def targets(action):
   yield '//gap/%s' % action
+  yield '//frete/check' % action
+  yield '//station/check' % action
   if osyst.startswith('linux'):
     yield '//oracles/%s' % action
   else:
