ece9a82313fa48d5d013c3cd7412599a4801610d
Gaëtan Rochel <gaetan.rochel@infinit.io>
2014-02-26 14:00:09 +0100

    oracles.meta.server: Fixes display for Today and Yesterday date range..

--- oracles/meta/server/resources/js/main.js
+++ oracles/meta/server/resources/js/main.js
@@ -257,8 +257,11 @@ $(document).ready(function() {
       },
       function(start, end) {
         $('.transactions').empty().addClass('loading');
-        
-        $('#datepicker').html(start.format('MMMM D, YYYY') + ' &#8594; ' + end.format('MMMM D, YYYY'));
+        if (start != end) {
+          $('#datepicker').html(start.format('MMMM D, YYYY') + ' &#8594; ' + end.format('MMMM D, YYYY'));
+        } else {
+          $('#datepicker').html(start.format('MMMM D, YYYY');
+        }
         var groups = getSelectedGroups();
         var status = getSelectedStatus();
         
