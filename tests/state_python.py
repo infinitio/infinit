c24d75ec5c1b4948f3dcf4885993f3948b76cc98
Christopher Crone <christopher.crone@infinit.io>
2014-03-14 14:44:10 -0400

    tests.State: Add tests for user list and transaction list.

--- tests/state_python.py
+++ tests/state_python.py
@@ -6,19 +6,50 @@ import unittest
 
 class Wrapper:
 
-  def login_logout_test(self, email='chris@infinit.io', password='password'):
+  def __init__(self, email='chris2@infinit.io', password='password'):
+    self.email = email
+    self.password = password
+
+  def _login(self):
     test_state = state.State('https', 'development.infinit.io', 443,
                              'development.infinit.io', 444)
-    hashed_password = state.State.hash_password(email, password)
-    test_state.login(email, hashed_password)
+    hashed_password = state.hash_password(self.email, self.password)
+    test_state.login(self.email, hashed_password)
+    return test_state
+
+  def login_logout_test(self, email='chris2@infinit.io', password='password'):
+    test_state = self._login()
     test_state.logout()
+
+  def user_list_test(self):
+    test_state = self._login()
+    users = test_state.users()
+    print(users)
+
+  def transaction_list_test(self):
+    test_state = self._login()
+    transactions = test_state.transactions()
+    print(transactions)
 
 class TestState(unittest.TestCase):
 
-  def test_login(self):
+  def test_login_logout(self):
     wrapper = Wrapper()
     sched = reactor.Scheduler()
     thread = reactor.Thread(sched, 'state', lambda: wrapper.login_logout_test())
+    sched.run()
+
+  def test_user_list(self):
+    wrapper = Wrapper()
+    sched = reactor.Scheduler()
+    thread = reactor.Thread(sched, 'state', lambda: wrapper.user_list_test())
+    sched.run()
+
+  def test_transaction_list(self):
+    wrapper = Wrapper()
+    sched = reactor.Scheduler()
+    thread = reactor.Thread(sched, 'state',
+                            lambda: wrapper.transaction_list_test())
     sched.run()
 
 unittest.main()
