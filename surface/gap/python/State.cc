97da887906faf1f060449dfb70d4d1f62a699471
Christopher Crone <christopher.crone@infinit.io>
2014-03-14 14:43:28 -0400

    surface.gap.State.python: Add transaction convertion.

--- surface/gap/python/State.cc
+++ surface/gap/python/State.cc
@@ -1,5 +1,7 @@
 #include <boost/python.hpp>
 
+#include <elle/assert.hh>
+
 #include <surface/gap/State.hh>
 
 #include <infinit/oracles/meta/Client.hh>
@@ -62,7 +64,7 @@ struct user_to_python_dict
 {
   static
   PyObject*
-  convert(infinit::oracles::meta::User user)
+  convert(infinit::oracles::meta::User const& user)
   {
     auto dict = PyDict_New();
     PyDict_SetItemString(dict, "id", PyUnicode_FromString(user.id.data()));
@@ -86,12 +88,12 @@ struct user_map_to_python_dict
 {
   static
   PyObject*
-  convert(std::unordered_map<unsigned int, infinit::oracles::meta::User> user_map)
+  convert(std::unordered_map<unsigned int, infinit::oracles::meta::User> const&
+          user_map)
   {
     auto dict = PyDict_New();
-    for (auto obj: user_map)
+    for (auto const& obj: user_map)
     {
-
       PyDict_SetItem(dict, PyLong_FromLong(obj.first),
                      user_to_python_dict::convert(obj.second));
     }
@@ -99,17 +101,116 @@ struct user_map_to_python_dict
   }
 };
 
-// struct transaction_to_python_dict
-// {
-//   static
-//   PyObject*
-//   convert(infinit::oracles::Transaction transaction)
-//   {
-//     auto dict = PyDict_New();
-//     PyDict_SetItemString(dict, "id",
-//                          PyUnicode_FromString(transaction.id.data()));
-//   }
-// };
+static
+std::string
+transaction_status_string(infinit::oracles::Transaction::Status status)
+{
+  using infinit::oracles::Transaction;
+  switch (status)
+  {
+    case Transaction::Status::failed:
+      return "failed";
+    case Transaction::Status::started:
+      return "started";
+    case Transaction::Status::created:
+      return "created";
+    case Transaction::Status::rejected:
+      return "rejected";
+    case Transaction::Status::canceled:
+      return "canceled";
+    case Transaction::Status::initialized:
+      return "initialized";
+    case Transaction::Status::finished:
+      return "finished";
+    case Transaction::Status::accepted:
+      return "accepted";
+    case Transaction::Status::none:
+      return "none";
+
+    default:
+      elle::unreachable();
+  }
+}
+
+struct transaction_to_python_dict
+{
+  static
+  PyObject*
+  convert(surface::gap::Transaction const& transaction)
+  {
+    auto dict = PyDict_New();
+    PyDict_SetItemString(
+      dict, "id",
+      PyUnicode_FromString(transaction.data()->id.data()));
+    PyDict_SetItemString(
+      dict, "sender_id",
+      PyUnicode_FromString(transaction.data()->sender_id.data()));
+    PyDict_SetItemString(
+      dict, "sender_fullname",
+      PyUnicode_FromString(transaction.data()->sender_fullname.data()));
+    PyDict_SetItemString(
+      dict, "sender_device_id",
+      PyUnicode_FromString(transaction.data()->sender_device_id.data()));
+    PyDict_SetItemString(
+      dict, "recipient_id",
+      PyUnicode_FromString(transaction.data()->recipient_id.data()));
+    PyDict_SetItemString(
+      dict, "recipient_fullname",
+      PyUnicode_FromString(transaction.data()->recipient_fullname.data()));
+    PyDict_SetItemString(
+      dict, "recipient_device_id",
+      PyUnicode_FromString(transaction.data()->recipient_device_id.data()));
+    PyDict_SetItemString(
+      dict, "recipient_device_name",
+      PyUnicode_FromString(transaction.data()->recipient_device_name.data()));
+    PyDict_SetItemString(
+      dict, "message",
+      PyUnicode_FromString(transaction.data()->message.data()));
+    auto file_list = PyList_New(transaction.data()->files.size());
+    for (auto file: transaction.data()->files)
+    {
+      PyList_Append(file_list, PyUnicode_FromString(file.data()));
+    }
+    PyDict_SetItemString(dict, "files", file_list);
+    PyDict_SetItemString(
+      dict, "files_count",
+      PyLong_FromLong(transaction.data()->files_count));
+    PyDict_SetItemString(
+      dict, "total_size",
+      PyLong_FromLong(transaction.data()->total_size));
+    PyDict_SetItemString(
+      dict, "is_directory",
+      PyBool_FromLong(transaction.data()->is_directory));
+    PyDict_SetItemString(
+      dict, "status",
+      PyUnicode_FromString(
+        transaction_status_string(transaction.data()->status).data()));
+    PyDict_SetItemString(
+      dict, "ctime",
+      PyFloat_FromDouble(transaction.data()->ctime));
+    PyDict_SetItemString(
+      dict, "mtime",
+      PyFloat_FromDouble(transaction.data()->mtime));
+    return dict;
+  }
+};
+
+struct transaction_map_to_python_dict
+{
+  static
+  PyObject*
+  convert(std::unordered_map<unsigned int, surface::gap::Transaction> const&
+    transaction_map)
+  {
+    auto dict = PyDict_New();
+    for (auto const& obj: transaction_map)
+    {
+      PyDict_SetItem(dict, PyLong_FromLong(obj.first),
+                     transaction_to_python_dict::convert(obj.second));
+    }
+    return dict;
+  }
+};
 
 static
 void
@@ -124,10 +225,21 @@ bind_user()
 
   boost::python::to_python_converter<
     const std::unordered_map<unsigned int, User>, user_map_to_python_dict>();
+
+  using surface::gap::Transaction;
+
+  boost::python::to_python_converter<const Transaction,
+                                   transaction_to_python_dict>();
+  boost::python::to_python_converter<
+    const std::unordered_map<unsigned int, Transaction>,
+    transaction_map_to_python_dict>();
 }
 
 BOOST_PYTHON_MODULE(state)
 {
+  namespace py = boost::python;
+  typedef
+    py::return_value_policy<boost::python::copy_const_reference> by_const_ref;
   bind_user();
   using surface::gap::State;
   boost::python::class_<State, boost::noncopyable>
@@ -140,8 +252,11 @@ BOOST_PYTHON_MODULE(state)
     .def("logged_in", &State::logged_in)
     .def("login", &State::login)
     .def("logout", &State::logout)
-    .def("hash_password", &State::hash_password)
-    .def("users", &State::users,
-      boost::python::return_value_policy<boost::python::copy_const_reference>())
+    .def("users", &State::users, by_const_ref())
+    .def("transactions", static_cast<
+      State::Transactions const& (State::*)() const>(&State::transactions),
+         by_const_ref())
     ;
+    // Static functions.
+    boost::python::def("hash_password", &State::hash_password);
 }
