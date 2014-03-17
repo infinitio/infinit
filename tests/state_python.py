f9ad10955853b6f4c5024392352524a37eb0df2c
Christopher Crone <christopher.crone@infinit.io>
2014-03-17 12:01:45 -0400

    surface.gap.State.python.test: Add transaction notification callback test and refactor.

--- tests/state_python.py
+++ tests/state_python.py
@@ -10,26 +10,40 @@ class Wrapper:
     self.email = email
     self.password = password
 
-  def _login(self):
+  def _transaction_callback(self, notification):
+    print('transaction_id=%s status=%s' % (notification['transaction_id'], notification['status']))
+
+  def _initialise_state(self,
+                        transaction_callback=False):
     test_state = state.State('https', 'development.infinit.io', 443,
                              'development.infinit.io', 444)
-    hashed_password = state.hash_password(self.email, self.password)
-    test_state.login(self.email, hashed_password)
+    if transaction_callback:
+      test_state.attach_transaction_callback(self._transaction_callback)
     return test_state
 
+  def _login(self, test_state):
+    hashed_password = state.hash_password(self.email, self.password)
+    test_state.login(self.email, hashed_password)
+
   def login_logout_test(self, email='chris2@infinit.io', password='password'):
-    test_state = self._login()
+    test_state = self._initialise_state()
+    self._login(test_state)
     test_state.logout()
 
   def user_list_test(self):
-    test_state = self._login()
+    test_state = self._initialise_state()
+    self._login(test_state)
     users = test_state.users()
     print(users)
 
   def transaction_list_test(self):
-    test_state = self._login()
+    test_state = self._initialise_state()
+    self._login(test_state)
     transactions = test_state.transactions()
     print(transactions)
+
+  def callback_test(self):
+    test_state = self._initialise_state(transaction_callback=True)
 
 class TestState(unittest.TestCase):
 
@@ -50,6 +64,12 @@ class TestState(unittest.TestCase):
     sched = reactor.Scheduler()
     thread = reactor.Thread(sched, 'state',
                             lambda: wrapper.transaction_list_test())
+    sched.run()
+
+  def test_callbacks(self):
+    wrapper = Wrapper()
+    sched = reactor.Scheduler()
+    thread = reactor.Thread(sched, 'state', lambda: wrapper.callback_test())
     sched.run()
 
 unittest.main()
