9e46056d5977d908cd62fcb8bd9b72c450be71b7
mefyl <quentin.hocquet@infinit.io>
2014-03-20 10:43:16 -0400

    tests: Restore basic functional tests.

--- tests/basic.py
+++ tests/basic.py
@@ -1,16 +1,14 @@
 #! /usr/bin/env python3
 
-import test
+import datetime
+import infinit.oracles.servers
 import reactor
 import time
-def go():
-  with test.Oracles() as o:
-    time.sleep(2)
 
+def main():
+  with infinit.oracles.servers.Oracles() as o:
+    reactor.sleep(datetime.timedelta(seconds = 2))
 
 s = reactor.Scheduler()
-t = reactor.Thread(s, "test starter", lambda : go())
+t = reactor.Thread(s, 'main', main)
 s.run()
-
-
-
