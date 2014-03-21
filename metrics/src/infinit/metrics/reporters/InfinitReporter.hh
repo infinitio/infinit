2ef08687b5a8cc9f67fd7eb7e1ad53fea4840d65
mefyl <quentin.hocquet@infinit.io>
2014-03-21 17:47:15 -0400

    metrics: Factor JSON reporter.

--- metrics/src/infinit/metrics/reporters/InfinitReporter.hh
+++ metrics/src/infinit/metrics/reporters/InfinitReporter.hh
@@ -1,107 +1,45 @@
 #ifndef INFINIT_METRICS_INFINIT_REPORTER_HH
 # define INFINIT_METRICS_INFINIT_REPORTER_HH
 
-# include <elle/json/json.hh>
-
-# include <infinit/metrics/Reporter.hh>
+# include <infinit/metrics/reporters/JSONReporter.hh>
 
 namespace infinit
 {
   namespace metrics
   {
-    enum class InfinitKey
-    {
-      connection_method,
-      event,
-      fail_reason,
-      file_count,
-      how_ended,
-      message_length,
-      metric_sender_id,
-      recipient_id,
-      sender_id,
-      status,
-      timestamp,
-      total_size,
-      transaction_id,
-      user_agent,
-      version,
-      who,
-    };
-
     class InfinitReporter:
-      public Reporter
+      public JSONReporter
     {
+    /*------.
+    | Types |
+    `------*/
+    public:
+      typedef JSONReporter Super;
+      typedef InfinitReporter Self;
+
+    /*-------------.
+    | Construction |
+    `-------------*/
     public:
       InfinitReporter();
-
       virtual
       ~InfinitReporter() = default;
 
-    /// Implementation of transaction metrics.
-    private:
+    /*-----.
+    | Send |
+    `-----*/
+    protected:
       virtual
       void
-      _transaction_accepted(std::string const& transaction_id);
+      _post(std::string const& destination,
+            elle::json::Object data) override;
 
-      virtual
-      void
-      _transaction_connected(std::string const& transaction_id,
-                             std::string const& connection_method);
-
-      virtual
-      void
-      _transaction_created(std::string const& transaction_id,
-                           std::string const& sender_id,
-                           std::string const& recipient_id,
-                           int64_t file_count,
-                           int64_t total_size,
-                           uint32_t message_length);
-
-      virtual
-      void
-      _transaction_ended(std::string const& transaction_id,
-                         infinit::oracles::Transaction::Status status,
-                         std::string const& info);
-
-    /// Implementation of user metrics.
-    private:
-      virtual
-      void
-      _user_favorite(std::string const& user_id);
-
-      virtual
-      void
-      _user_login(bool success, std::string const& info);
-
-      virtual
-      void
-      _user_logout(bool success, std::string const& info);
-
-      virtual
-      void
-      _user_unfavorite(std::string const& user_id);
-
-    /// Private helper functions.
-    private:
-      std::string
-      _key_str(InfinitKey k);
-
-      void
-      _send(std::string const& destination, elle::json::Object data);
-
-      std::string
-      _status_string(bool success);
-
-      std::string
-      _transaction_status_str(infinit::oracles::Transaction::Status status);
-
-    /// Private attributes.
+    /*----------------------.
+    | Connection attributes |
+    `----------------------*/
     private:
       ELLE_ATTRIBUTE(std::string, base_url);
       ELLE_ATTRIBUTE(int, port);
-      ELLE_ATTRIBUTE(std::string, transaction_dest);
-      ELLE_ATTRIBUTE(std::string, user_dest);
     };
   }
 }
