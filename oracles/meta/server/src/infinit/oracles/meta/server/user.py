a388e7abddcc5f07d31c22b0c0906036ac92c23d
Christopher Crone <christopher.crone@infinit.io>
2014-04-01 17:03:11 -0400

    oracles.server.meta.user: Use an aggregation for email searching.

--- oracles/meta/server/src/infinit/oracles/meta/server/user.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/user.py
@@ -422,25 +422,19 @@ class Mixin:
     """
     with elle.log.trace("%s: search %s emails (limit: %s, offset: %s)" %
                         (self.user['_id'], len(emails), limit, offset)):
-      fields = {
-        'id': '$_id',
-        'email' : '$email',
-        'public_key': '$public_key',
-        'fullname': '$fullname',
-        'handle': '$handle',
-        'connected_devices': '$connected_devices',
-        'status': '$status',
-      }
-      res = self.database.users.find(
+      res = self.database.users.aggregate([
         {
-          'email': {'$in': emails},
-          'register_status': 'ok',
+          '$match':
+          {
+            'email': {'$in': emails},
+            'register_status': 'ok',
+          }
         },
-        fields = fields,
-        limit = limit,
-        skip = offset,
-      )
-      return {'users': list(res)}
+        {'$limit': limit},
+        {'$skip': offset},
+        {'$project': dict(email = '$email', **self.user_public_fields)}
+      ])
+      return {'users': res['result']}
 
   @api('/user/search', method = 'POST')
   # XXX: This call is used by the waterfall which does not login. We need to
