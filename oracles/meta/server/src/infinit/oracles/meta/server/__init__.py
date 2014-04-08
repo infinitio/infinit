23052e2f1d78d483c5d6ff1464e02a925e4d55fd
mefyl <quentin.hocquet@infinit.io>
2014-04-08 02:13:43 -0400

    oracles.meta.server: Escape backslashes in literal string.

--- oracles/meta/server/src/infinit/oracles/meta/server/__init__.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/__init__.py
@@ -280,7 +280,7 @@ class Meta(bottle.Bottle,
       # Try to distinguish browser user agent from meta client.
       # This assume that python re will take the complete subminor and not stop
       # at first digit found.
-      pattern = re.compile('MetaClient/\d+\.\d+\.\d+')
+      pattern = re.compile('MetaClient/\\d+\\.\\d+\\.\\d+')
       res = re.match(pattern, self.user_agent)
       if res is None:
         elle.log.debug('can\'t extract version from user agent %s' %
