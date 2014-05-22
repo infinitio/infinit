638dccc927c68e03602cb514068e394177784d9e
Christopher Crone <christopher.crone@infinit.io>
2014-05-21 21:57:51 -0400

    oracles.meta.server.wsgi: Correct production links bucket name.

--- oracles/meta/server/src/infinit/oracles/meta/server/wsgi.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/wsgi.py
@@ -2,4 +2,4 @@ import infinit.oracles.meta.server
 
 application = infinit.oracles.meta.server.Meta(aws_region = 'us-east-1',
                                                aws_buffer_bucket = 'us-east-1-buffer-infinit-io',
-                                               aws_link_bucket = 'us-east-1-link-infinit-io')
+                                               aws_link_bucket = 'us-east-1-links-infinit-io')
