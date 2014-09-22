b4a3155e1cc2e37458e740855bdb7d0cafb50e49
mefyl <quentin.hocquet@infinit.io>
2014-09-22 18:07:16 +0200

    oracles.meta.server: Add require_key.

--- oracles/meta/server/src/infinit/oracles/meta/server/utils.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/utils.py
@@ -1,8 +1,12 @@
+import bottle
 import decorator
+import inspect
 import pymongo
-from infinit.oracles.meta.server import conf
 
-from infinit.oracles.utils import api, json_value, utf8_string
+from itertools import chain
+
+from infinit.oracles.meta.server import conf
+from infinit.oracles.utils import api, json_value, utf8_string, key
 
 def require_logged_in(method):
   if hasattr(method, '__api__'):
@@ -33,6 +37,28 @@ def require_logged_in_or_admin(method):
       self.forbidden()
     return wrapped(self, *args, **kwargs)
   return decorator.decorator(wrapper, method)
+
+def require_key(method):
+  if hasattr(method, '__api__'):
+    raise Exception(
+      'require_key for %r wraps the API' % method.__name__)
+  def require_key(self, *args, **kwargs):
+    import sys
+    if 'key' not in kwargs:
+      # For now, this will be refused by the API as the 'key'
+      # parameter is missing.
+      self.forbidden()
+    if kwargs['key'] != key(bottle.request.path):
+      self.forbidden()
+    del kwargs['key']
+    return method(self, *args, **kwargs)
+  spec = inspect.getfullargspec(method)
+  if 'key' in spec.args:
+    raise Exception(
+      'require_key method already has a \'key\' argument')
+  spec.args.insert(1, 'key')
+  require_key.__fullargspec__ = spec
+  return require_key
 
 def hash_pasword(password):
   import hashlib
