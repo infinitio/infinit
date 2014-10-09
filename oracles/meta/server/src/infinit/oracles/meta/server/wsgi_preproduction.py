f6e6938181cfcf4735e86fb29b3fb1b1f0201b80
mefyl <quentin.hocquet@infinit.io>
2014-10-09 12:44:36 +0200

    oracles.meta.server: Fix preproduction mongo hosts.

--- oracles/meta/server/src/infinit/oracles/meta/server/wsgi_preproduction.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/wsgi_preproduction.py
@@ -5,7 +5,7 @@ os.environ['META_LOG_SYSTEM'] = 'meta'
 os.environ['ELLE_LOG_LEVEL'] = 'infinit.oracles.meta.*:DEBUG'
 
 application = infinit.oracles.meta.server.Meta(
-  mongo_replica_set = ['0.mongo.production.infinit.io'],
+  mongo_replica_set = ['mongo-0', 'mongo-1', 'mongo-2',],
   aws_region = 'us-east-1',
   aws_buffer_bucket = 'us-east-1-buffer-infinit-io',
   aws_link_bucket = 'us-east-1-links-infinit-io',
