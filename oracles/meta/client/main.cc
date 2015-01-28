604ace76089bff95824300cbbc4e675f7fccb192
mefyl <quentin.hocquet@infinit.io>
2015-01-28 11:30:26 +0100

    oracles.meta.client: Print out responses.

--- oracles/meta/client/main.cc
+++ oracles/meta/client/main.cc
@@ -6,6 +6,7 @@
 
 #include <elle/Error.hh>
 #include <elle/log.hh>
+#include <elle/serialization/json.hh>
 #include <elle/utility/Utility.hh>
 
 #include <reactor/scheduler.hh>
@@ -16,23 +17,6 @@
 ELLE_LOG_COMPONENT("oracles.meta.client");
 
 using infinit::oracles::meta::Client;
-
-template <typename R, typename ... Args>
-struct Invoke
-{
-  template <typename M, typename ... Passed>
-  static
-  void
-  invoke(Client& client,
-         M m,
-         std::vector<std::string> const& args,
-         Passed&& ... passed)
-  {
-    if (!args.empty())
-      throw elle::Error("too many arguments");
-    (client.*m)(std::forward<Passed>(passed)...);
-  }
-};
 
 template <typename T>
 struct Convert
@@ -76,6 +60,40 @@ struct Convert<boost::uuids::uuid>
   }
 };
 
+template <typename R, typename ... Args>
+struct Invoke
+{
+  template <typename M, typename ... Passed>
+  static
+  void
+  invoke(Client& client,
+         M m,
+         std::vector<std::string> const& args,
+         Passed&& ... passed)
+  {
+    if (!args.empty())
+      throw elle::Error("too many arguments");
+    std::cout << (client.*m)(std::forward<Passed>(passed)...) << std::endl;
+  }
+};
+
+template <typename ... Args>
+struct Invoke<void, Args ...>
+{
+  template <typename M, typename ... Passed>
+  static
+  void
+  invoke(Client& client,
+         M m,
+         std::vector<std::string> const& args,
+         Passed&& ... passed)
+  {
+    if (!args.empty())
+      throw elle::Error("too many arguments");
+    (client.*m)(std::forward<Passed>(passed)...);
+  }
+};
+
 template <typename R, typename Head, typename ... Tail>
 struct Invoke<R, Head, Tail ...>
 {
@@ -102,6 +120,26 @@ struct Invoke<R, Head, Tail ...>
   }
 };
 
+static
+std::ostream&
+operator << (std::ostream& s,
+             infinit::oracles::meta::LoginResponse const& r)
+{
+  elle::serialization::json::SerializerOut output(s, false);
+  output.serialize_forward(r);
+  return s;
+}
+
+static
+std::ostream&
+operator << (std::ostream& s,
+             infinit::oracles::meta::LoginResponse::Trophonius const& r)
+{
+  elle::serialization::json::SerializerOut output(s, false);
+  output.serialize_forward(r);
+  return s;
+}
+
 template <typename R, typename ... Args>
 static
 void
@@ -116,11 +154,39 @@ register_method(boost::program_options::options_description& options,
     ->notifier(
       [m, name, &actions] (std::vector<std::string> args)
       {
-        ELLE_TRACE_SCOPE("invoke %s(%s)", name, args);
-        actions.push_back([args, m] (Client& c)
-                          {
-                            Invoke<R, Args...>::invoke(c, m, args);
-                          });
+        ELLE_TRACE_SCOPE("enqueue %s(%s)", name, args);
+        actions.push_back(
+          [name, args, m] (Client& c)
+          {
+            ELLE_TRACE_SCOPE("invoke %s(%s)", name, args);
+            Invoke<R, Args...>::invoke(c, m, args);
+          });
+      })
+    );
+}
+
+template <typename R, typename ... Args>
+static
+void
+register_method(boost::program_options::options_description& options,
+                std::string name,
+                R (Client::*m)() const,
+                std::vector<std::function<void (Client&)>>& actions)
+{
+  options.add_options()(
+    name.c_str(),
+    boost::program_options::value<std::vector<std::string>>()
+    ->implicit_value(std::vector<std::string>())->zero_tokens()
+    ->notifier(
+      [m, name, &actions] (std::vector<std::string> args)
+      {
+        ELLE_TRACE_SCOPE("enqueue %s(%s)", name, args);
+        actions.push_back(
+          [name, args, m] (Client& c)
+          {
+            ELLE_TRACE_SCOPE("invoke %s(%s)", name, args);
+            Invoke<R, Args...>::invoke(c, m, args);
+          });
       })
     );
 }
@@ -157,8 +223,10 @@ parse_options(int argc, char** argv,
       "meta host"
     )
     ;
-  register_method(options, "register", &Client::register_, actions);
   register_method(options, "login", &Client::login, actions);
+  register_method(options, "logout", &Client::logout, actions);
+  register_method(options, "register", &Client::register_, actions);
+  register_method(options, "trophonius", &Client::trophonius, actions);
   variables_map vm;
   try
   {
