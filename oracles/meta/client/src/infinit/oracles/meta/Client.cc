f907a9b7845b7cf5d5fefc89d99e6b3b217dc4aa
Christopher Crone <christopher.crone@infinit.io>
2015-03-04 17:46:20 +0100

    oracles.meta.client: Send transaction recipient device on reject.

--- oracles/meta/client/src/infinit/oracles/meta/Client.cc
+++ oracles/meta/client/src/infinit/oracles/meta/Client.cc
@@ -907,7 +907,8 @@ namespace infinit
                             const_cast<std::string&>(transaction_id));
             int status_integral = static_cast<int>(status);
             query.serialize("status", status_integral);
-            if (status == oracles::Transaction::Status::accepted)
+            if (status == oracles::Transaction::Status::accepted ||
+                status == oracles::Transaction::Status::rejected)
             {
               ELLE_ASSERT_GT(device_id.length(), 0u);
               ELLE_ASSERT_GT(device_name.length(), 0u);
