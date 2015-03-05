9da1d8e04f506478ebeb7540bb3f68783facab6e
mefyl <quentin.hocquet@infinit.io>
2015-03-05 16:05:40 +0100

    surface.gap.Peer{Send,Receive}Machine: Fix transfer state name in snapshots.

--- fist/src/surface/gap/PeerReceiveMachine.cc
+++ fist/src/surface/gap/PeerReceiveMachine.cc
@@ -147,7 +147,7 @@ namespace surface
             this->_run(this->_finish_state);
           else if (snapshot.current_state() == "reject")
             this->_run(this->_reject_state);
-          else if (snapshot.current_state() == "transfer core")
+          else if (snapshot.current_state() == "transfer")
             this->_run(this->_transfer_state);
           else if (snapshot.current_state() == "wait for decision")
             this->_run(this->_wait_for_decision_state);
