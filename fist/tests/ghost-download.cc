eaabf89545cdf3abe19a0b1afe70ae26473b738b
mefyl <quentin.hocquet@infinit.io>
2015-03-11 16:05:13 +0100

    surface.gap: Add test for a ghost waiting for data.

--- fist/tests/ghost-download.cc
+++ fist/tests/ghost-download.cc
@@ -9,7 +9,7 @@
 
 ELLE_LOG_COMPONENT("surface.gap.State.test");
 
-// Send a file to a ghost.
+// Download a ghost invite.
 ELLE_TEST_SCHEDULED(ghost_download)
 {
   // FIXME: check statuses sent to meta
@@ -29,17 +29,14 @@ ELLE_TEST_SCHEDULED(ghost_download)
   auto& sender = server.register_user("sender@infinit.io", password);
   auto& user = server.register_user(email, password);
   tests::State state(server, elle::UUID::random());
-
   auto t = std::make_shared<tests::Transaction>();
   t->recipient_id = user.id().repr();
   t->sender_id = sender.id().repr();
   t->is_ghost = true;
   t->download_link =
     elle::sprintf("http://127.0.0.1:%s/ghost-download", server.port());
-  // FIXME: also test changing to ghost uploaded later
   t->status = infinit::oracles::Transaction::Status::ghost_uploaded;
   server.transactions().insert(t);
-
   reactor::Barrier accepted;
   reactor::Barrier transferring;
   reactor::Barrier finished;
@@ -85,9 +82,66 @@ ELLE_TEST_SCHEDULED(ghost_download)
   }
 }
 
+// Fetch a not-yet-completed ghost upload.
+ELLE_TEST_SCHEDULED(wait_for_data)
+{
+  tests::Server server;
+  auto const email = "em@il.com";
+  auto const password = "secret";
+  auto& sender = server.register_user("sender@infinit.io", password);
+  auto& user = server.register_user(email, password);
+  tests::State state(server, elle::UUID::random());
+  auto t = std::make_shared<tests::Transaction>();
+  t->recipient_id = user.id().repr();
+  t->sender_id = sender.id().repr();
+  t->is_ghost = true;
+  t->download_link =
+    elle::sprintf("http://127.0.0.1:%s/ghost-download", server.port());
+  t->status = infinit::oracles::Transaction::Status::initialized;
+  server.transactions().insert(t);
+  reactor::Barrier waited;
+  reactor::Barrier finished;
+  state->attach_callback<surface::gap::PeerTransaction>(
+    [&] (surface::gap::PeerTransaction const& transaction)
+    {
+      // Check the GUI goes created -> transferring -> finished.
+      auto status = transaction.status;
+      ELLE_LOG("new transaction status: %s", status);
+      switch (status)
+      {
+        case gap_transaction_waiting_data:
+          BOOST_CHECK(!waited);
+          BOOST_CHECK(!finished);
+          ELLE_LOG("complete upload")
+          {
+            auto t = state->transactions().at(transaction.id).get();
+            t->data()->status =
+              infinit::oracles::Transaction::Status::ghost_uploaded;
+            t->on_transaction_update(t->data());
+          }
+          waited.open();
+          break;
+        case gap_transaction_waiting_accept:
+          BOOST_CHECK(waited);
+          BOOST_CHECK(!finished);
+          finished.open();
+          break;
+        default:
+          BOOST_ERROR(elle::sprintf("unexpected GAP status: %s", status));
+      }
+    });
+  state->login(email, password);
+  while (!finished)
+  {
+    reactor::sleep(100_ms);
+    state->poll();
+  }
+}
+
 ELLE_TEST_SUITE()
 {
   auto timeout = RUNNING_ON_VALGRIND ? 60 : 15;
   auto& suite = boost::unit_test::framework::master_test_suite();
   suite.add(BOOST_TEST_CASE(ghost_download), 0, timeout);
+  suite.add(BOOST_TEST_CASE(wait_for_data), 0, timeout);
 }
