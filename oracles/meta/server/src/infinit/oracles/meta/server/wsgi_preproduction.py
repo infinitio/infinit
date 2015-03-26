ad950b390ea0b11a45a8518cff9c252ffd01ed6a
mefyl <quentin.hocquet@infinit.io>
2015-03-26 15:51:24 +0100

    oracles.meta.server: Enable SendWithUs emails on WSGI adaptors.

--- oracles/meta/server/src/infinit/oracles/meta/server/wsgi_preproduction.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/wsgi_preproduction.py
@@ -4,11 +4,15 @@ import os
 os.environ['META_LOG_SYSTEM'] = 'meta'
 os.environ['ELLE_LOG_LEVEL'] = 'infinit.oracles.meta.*:DEBUG'
 
+swu_key = 'live_7e775f6f0e1404802a5fbbc0fcfa9c238b065c49'
+emailer = infinit.oracles.emailer.SendWithUsEmailer(api_key = swu_key)
+
 application = infinit.oracles.meta.server.Meta(
   mongo_replica_set = ['mongo-0', 'mongo-1', 'mongo-2',],
   aws_region = 'us-east-1',
   aws_buffer_bucket = 'us-east-1-buffer-infinit-io',
   aws_link_bucket = 'us-east-1-links-infinit-io',
   production = True,
+  emailer = emailer,
   zone = 'preproduction',
 )
