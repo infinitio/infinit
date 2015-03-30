dbfad95e6aa24be53d089ee682111bee1ca8c04d
Christopher Crone <christopher.crone@infinit.io>
2015-03-30 11:24:36 +0200

    oracles.sisyphis.apns: Add metric on invalidate token.

--- oracles/sisyphus/src/infinit/oracles/sisyphus/boulders/apns.py
+++ oracles/sisyphus/src/infinit/oracles/sisyphus/boulders/apns.py
@@ -18,17 +18,28 @@ class APNSUnregister(Boulder):
     for token, fail_time in self.__apns.feedback_server.items():
       token = token.decode('utf-8')
       elle.log.debug('%s: drop invalid token %s' % (self, token))
-      self.sisyphus.mongo.meta.users.update(
-        {
+      user = self.sisyphus.mongo.meta.users.find_and_modify(
+        query = {
           'devices.push_token': token,
         },
-        {
+        update = {
           '$unset':
           {
             'devices.$.push_token': True,
           }
+        },
+        fields = {
+          '_id': True,
         }
       )
+      if self.sisyphus.metrics is not None:
+        res = self.sisyphus.metrics.send(
+          {
+            'event': 'push token invalidated',
+            'timestamp': time.time(),
+            'fail_time': fail_time,
+            'user': str(user['_id']),
+          })
 
   def status(self):
     return {}
