e239ec288ccc35718387dabbc1d1705d6c8ccc05
Kévin Sztern <kevin.sztern@infinit.io>
2015-05-06 18:43:13 +0200

    surface.gap.PeerSendMachine: Add transition from wait_for_accept to _pausing.

--- fist/src/surface/gap/PeerSendMachine.cc
+++ fist/src/surface/gap/PeerSendMachine.cc
@@ -106,6 +106,11 @@ namespace surface
         this->_end_state,
         reactor::Waitables{&this->rejected()},
         true);
+      this->_machine.transition_add(
+        this->_wait_for_accept_state,
+        this->_pausing_state,
+        reactor::Waitables{&this->transaction().pausing()},
+        true);
       this->transaction_status_update(data->status);
     }
 
@@ -209,6 +214,10 @@ namespace surface
           this->_run(this->_wait_for_accept_state);
         else if (snapshot.current_state() == "another device")
           this->_run(this->_another_device_state);
+        else if (snapshot.current_state() == "pausing")
+          this->_run(this->_pausing_state);
+        else if (snapshot.current_state() == "paused")
+          this->_run(this->_paused_state);
         else
         {
           ELLE_WARN("%s: unkown state in snapshot: %s",
