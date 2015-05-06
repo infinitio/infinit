bb720b175d52ea72c4d81b644d7b1e215b76d423
mefyl <quentin.hocquet@infinit.io>
2015-05-06 17:33:56 +0200

    oracles.meta.server: Enable to pass the session-id in the query string.

--- oracles/meta/server/src/infinit/oracles/meta/server/plugins/session.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/plugins/session.py
@@ -34,6 +34,14 @@ class Plugin(object):
                                       secret = Plugin.secret)
       bottle.request.session_id = sid
       bottle.request.session = {}
+      if sid is None and 'session-id' in bottle.request.query:
+        sid = bottle.cookie_decode(bottle.request.query['session-id'],
+                                   Plugin.secret)[1]
+        del bottle.request.query['session-id']
+        bottle.response.set_cookie(Plugin.key,
+                                   sid,
+                                   secret = Plugin.secret,
+                                   max_age = 3600 * 24 * 365)
       if sid is not None:
         session = collection.find_one({'_id': sid})
         if session is not None:
