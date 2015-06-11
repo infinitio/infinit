5fec57d77b6f9123cabd1528c21891071aba041f
Christopher Crone <christopher.crone@infinit.io>
2015-06-11 12:47:27 +0200

    fist.surface.gap.Model: Add user accounts.

--- fist/src/surface/gap/Model.hh
+++ fist/src/surface/gap/Model.hh
@@ -3,28 +3,43 @@
 
 # include <das/model.hh>
 
+# include <infinit/oracles/meta/Account.hh>
 # include <infinit/oracles/meta/Device.hh>
 
 namespace surface
 {
   namespace gap
   {
+    typedef infinit::oracles::meta::Account Account;
     typedef infinit::oracles::meta::Device Device;
 
     class Model
     {
     public:
+      das::IndexList<Account, std::string, &Account::id> accounts;
       das::IndexList<Device, elle::UUID, &Device::id> devices;
     };
   }
 }
 
+DAS_MODEL_FIELD(surface::gap::Model, accounts);
 DAS_MODEL_FIELD(surface::gap::Model, devices);
 
 namespace surface
 {
   namespace gap
   {
+    typedef das::Object<
+      Account,
+      das::Field<Account, std::string, &Account::id>,
+      das::Field<Account, std::string, &Account::type>
+      > DasAccount;
+    typedef das::Collection<
+      Account,
+      std::string,
+      &Account::id,
+      DasAccount
+      > DasAccounts;
     typedef das::Object<
       Device,
       das::Field<Device, elle::UUID, &Device::id>,
@@ -43,7 +58,11 @@ namespace surface
       das::Field<Model,
                  das::IndexList<Device, elle::UUID, &Device::id>,
                  &Model::devices,
-                 DasDevices>
+                 DasDevices>,
+      das::Field<Model,
+                 das::IndexList<Account, std::string, &Account::id>,
+                 &Model::accounts,
+                 DasAccounts>
       > DasModel;
   }
 }
