e1f8dc88c6819a3281bc5cfc213c373ede79d15e
Antony MECHIN <antony.mechin@infinit.io>
2015-07-08 14:48:42 +0200

    oracles.meta.server.user: Deprecate Windows users with older version than 0.9.39.

--- oracles/meta/server/src/infinit/oracles/meta/server/user.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/user.py
@@ -551,6 +551,11 @@ class Mixin:
     # FIXME: 0.0.0.0 is the website.
     if self.user_version < (0, 9, 0) and self.user_version != (0, 0, 0):
       return self.fail(error.DEPRECATED)
+    if self.user_version < (0, 9, 39) and OS == "Windows":
+      return self.forbidden({
+        'reason': 'Version is deprecated',
+        'code': error.DEPRECATED[0]
+      })
     user, res = self._login(
       email = email,
       fields = self.__user_self_fields + ['public_key'],
