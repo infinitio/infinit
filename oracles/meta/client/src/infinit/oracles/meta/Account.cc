83f490ac01aa4db4aa8d7e45c733b7766b698039
Antony MECHIN <antony.mechin@infinit.io>
2015-08-12 11:31:10 +0200

    oracles.meta.client.Account: Add quotas in account.

--- oracles/meta/client/src/infinit/oracles/meta/Account.cc
+++ oracles/meta/client/src/infinit/oracles/meta/Account.cc
@@ -1,3 +1,5 @@
+#include <elle/serialization/json/TypeError.hh>
+
 #include <infinit/oracles/meta/Account.hh>
 
 namespace infinit
@@ -22,6 +24,81 @@ namespace infinit
         }
       }
 
+      Account::Quotas::QuotaUsage::QuotaUsage(elle::serialization::SerializerIn& s)
+      {
+        this->serialize(s);
+      }
+
+      void
+      Account::Quotas::QuotaUsage::serialize(elle::serialization::Serializer& s)
+      {
+        // XXX: Remove the try catch when feature/json-reset-optionals is
+        // merged.
+        try
+        {
+          s.serialize("quota", this->quota);
+        }
+        catch (elle::serialization::TypeError const&)
+        {
+          this->quota.reset();
+        }
+        s.serialize("used", this->used);
+      }
+
+      void
+      Account::Quotas::QuotaUsage::print(std::ostream& stream) const
+      {
+        stream << "Quota(" << this->used << "/" << this->quota << ")";
+      }
+
+      Account::Quotas::Limit::Limit(elle::serialization::SerializerIn& s)
+      {
+        this->serialize(s);
+      }
+
+      void
+      Account::Quotas::Limit::serialize(elle::serialization::Serializer& s)
+      {
+        // XXX: Remove the try catch when feature/json-reset-optionals is
+        // merged.
+        try
+        {
+          s.serialize("limit", this->limit);
+        }
+        catch (elle::serialization::TypeError const&)
+        {
+          this->limit.reset();
+        }
+      }
+
+      void
+      Account::Quotas::Limit::print(std::ostream& stream) const
+      {
+        stream << "Limit(" << this->limit << ")";
+      }
+
+      Account::Quotas::Quotas(elle::serialization::SerializerIn& s)
+      {
+        this->serialize(s);
+      }
+
+      void
+      Account::Quotas::serialize(elle::serialization::Serializer& s)
+      {
+        s.serialize("send_to_self", this->send_to_self);
+        s.serialize("links", this->links);
+        s.serialize("p2p", this->p2p);
+      }
+
+      void
+      Account::Quotas::print(std::ostream& stream) const
+      {
+        stream << "Quotas("
+               << "send to self = " << this->send_to_self << ", "
+               << "links = " << this->links << ", "
+               << "p2p = " << this->p2p << ")";
+      }
+
       Account::Account(elle::serialization::SerializerIn& s)
       {
         this->serialize(s);
@@ -33,17 +110,15 @@ namespace infinit
         s.serialize("custom_domain", this->custom_domain);
         s.serialize("link_format", this->link_format);
         s.serialize("plan", this->plan);
-        s.serialize("link_size_quota", this->link_size_quota);
-        s.serialize("link_size_used", this->link_size_used);
+        s.serialize("quotas", this->quotas);
       }
 
       void
       Account::print(std::ostream& stream) const
       {
-        stream << "Account(" << account_type_string(this->plan)
-               << ", custom_domain = " << this->custom_domain
-               << ", link_size_used = " << this->link_size_used
-               << ", link_size_quota = " << this->link_size_quota << ")";
+        stream << "Account(" << account_type_string(this->plan) << ", "
+               << "custom_domain = " << this->custom_domain << ", "
+               << "quotas = " << this->quotas << ")";
       }
 
       std::ostream&
--- oracles/meta/client/src/infinit/oracles/meta/Account.hh
+++ oracles/meta/client/src/infinit/oracles/meta/Account.hh
@@ -2,6 +2,7 @@
 # define INFINIT_ORACLES_META_CLIENT_ACCOUNT_HH
 
 # include <elle/Printable.hh>
+# include <elle/optional.hh>
 
 # include <das/model.hh>
 
@@ -21,6 +22,106 @@ namespace infinit
       struct Account
         : public elle::Printable
       {
+        struct Quotas
+          : public elle::Printable
+        {
+          struct QuotaUsage
+            : public elle::Printable
+          {
+          /*-------------.
+          | Construction |
+          `-------------*/
+          public:
+            QuotaUsage(elle::serialization::SerializerIn& s);
+            QuotaUsage() = default;
+            virtual
+            ~QuotaUsage() = default;
+
+          /*--------------.
+          | Serialization |
+          `--------------*/
+          public:
+            void
+            serialize(elle::serialization::Serializer& s);
+
+          /*-----------.
+          | Attributes |
+          `-----------*/
+          public:
+            boost::optional<uint64_t> quota;
+            uint64_t used;
+
+          /*----------.
+          | Printable |
+          `----------*/
+          protected:
+            void
+            print(std::ostream& stream) const override;
+          };
+
+          struct Limit
+            : public elle::Printable
+          {
+          /*-------------.
+          | Construction |
+          `-------------*/
+          public:
+            Limit(elle::serialization::SerializerIn& s);
+            Limit() = default;
+            virtual
+            ~Limit() = default;
+
+          /*--------------.
+          | Serialization |
+          `--------------*/
+          public:
+            void
+            serialize(elle::serialization::Serializer& s);
+
+          /*-----------.
+          | Attributes |
+          `-----------*/
+          public:
+            boost::optional<uint64_t> limit;
+
+          /*----------.
+          | Printable |
+          `----------*/
+          protected:
+            void
+            print(std::ostream& stream) const override;
+          };
+
+        /*-------------.
+        | Construction |
+        `-------------*/
+        public:
+          Quotas(elle::serialization::SerializerIn& s);
+          Quotas() = default;
+          virtual
+          ~Quotas() = default;
+        /*--------------.
+        | Serialization |
+        `--------------*/
+        public:
+          void
+          serialize(elle::serialization::Serializer& s);
+
+        /*-----------.
+        | Attributes |
+        `-----------*/
+        public:
+          QuotaUsage send_to_self;
+          QuotaUsage links;
+          Limit p2p;
+
+        /*----------.
+        | Printable |
+        `----------*/
+        protected:
+          void
+          print(std::ostream& stream) const override;
+        };
       /*-------------.
       | Construction |
       `-------------*/
@@ -32,6 +133,7 @@ namespace infinit
         das::Variable<uint64_t> link_size_quota;
         das::Variable<uint64_t> link_size_used;
         das::Variable<AccountPlanType> plan;
+        das::Variable<Quotas> quotas;
 
       /*--------------.
       | Serialization |
@@ -57,9 +159,10 @@ namespace infinit
 
 # include <infinit/oracles/meta/Account.hxx>
 
-DAS_MODEL(infinit::oracles::meta::Account,
-          (custom_domain, link_format, plan, link_size_quota, link_size_used),
-          DasAccount);
+DAS_MODEL(
+  infinit::oracles::meta::Account,
+  (custom_domain, link_format, plan, link_size_quota, link_size_used, quotas),
+  DasAccount);
 DAS_MODEL_DEFAULT(infinit::oracles::meta::Account, DasAccount);
 
 #endif
