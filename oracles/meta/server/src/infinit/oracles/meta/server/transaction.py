8b08df7dcaca306db0940c8431ce702cf031e352
Antony MECHIN <antony.mechin@infinit.io>
2015-08-26 19:59:41 +0200

    Revert "oracles.meta.servers.transactions: Don't remove the download link until the website is ready."
    This reverts commit ce80308f72d109bf81148ca7ffa652cade7145a4.

--- oracles/meta/server/src/infinit/oracles/meta/server/transaction.py
+++ oracles/meta/server/src/infinit/oracles/meta/server/transaction.py
@@ -338,8 +338,7 @@ class Mixin:
         sender_premium = self.__user_id_premium(transaction['sender_id'])
         has_download_link = transaction.get('download_link')
         if no_ghost_downloads and has_download_link and not sender_premium:
-          pass # XXX: Wait for the website to be released.
-          # del transaction['download_link']
+          del transaction['download_link']
         return transaction
 
   # FIXME: Nuke this ! Use the user fetching routines from user.py and
